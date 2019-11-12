c401cdea6e02462c423850d04430476b42ba7a90
김세진 <sejjj120@gmail.com>
2019-11-12 18:57:59 +0900

    feat: koa 서버에 graphql 서버 추가

---
+++ server/api-gateway/index.js
@@ -0,0 +1,27 @@
+const Koa = require("koa");
+const KoaRouter = require("koa-router");
+const koaBody = require("koa-bodyparser");
+const { ApolloServer, gql } = require("apollo-server-koa");
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = {
+	Query: {
+		hello: () => "hello world"
+	}
+};
+const server = new ApolloServer({ typeDefs, resolvers });
+const app = new Koa();
+const router = new KoaRouter();
+
+app.use(koaBody());
+app.use(router.routes());
+app.use(router.allowedMethods());
+server.applyMiddleware({ app });
+
+app.listen(8000, () => {
+	console.log("8000번에서 API Gateway 실행중...")
+});
