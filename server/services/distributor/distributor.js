531cdc6f87252162bb6c5a502e332af16b1be30d
soob-forest <dltntnqo@naver.com>
2019-11-13 21:11:23 +0900

    feat(distributor): onRead함수 작성
    받은 packet을 handling하는 method.
    nodeList에는 distributor가 가지고 있는 모든 노드들의 정보가 있다.

--- server/services/distributor/distributor.js
+++ server/services/distributor/distributor.js
@@ -11,6 +11,36 @@ class Distributor extends TcpServer{
     super("distributor", "127.0.0.1", 8100, "distribute");
     instance = this;
   }
+
+  onRead(socket, data){
+    const key = makeKey(socket.remoteAddress + socket.remotePort);
+    const { method, query } = data;
+  
+    if(query !== "distribute"){
+      return
+    }
+    let packet;
+  
+    switch (method) {
+      case "GET" : 
+      packet = makePacket("POST", "nodes",{},{nodeList},this.context);
+      this.send(socket, packet)
+        break;
+      case "POST" : 
+      nodeList[key] = {};
+      nodeList[key].socket = socket;
+      nodeList[key].info = data.info;
+      packet = makePacket("POST", "nodes", {}, {nodeList}, this.context);
+        break;
+      case "DELETE" : 
+      delete nodeList[key];
+      packet = makePacket("POST", "nodes",{},{nodeList},this.context);
+        break;
+      default:
+        break;
+    }
+  
+  }
 }
 
 module.exports = Distributor;
