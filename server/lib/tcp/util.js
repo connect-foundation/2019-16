67f101ebe01a5a96ed6c0dc300dcefdb08dfb21b
soob-forest <dltntnqo@naver.com>
2019-11-13 23:32:43 +0900

    fix(lib/tcp/util): makeKey 함수 수정
    파라미터를 넣지 않고 있어서 파라미터를 넣어줌

--- server/lib/tcp/util.js
+++ server/lib/tcp/util.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 
-exports.makeKey = async() => {
+exports.makeKey = async(socket) => {
   const key = socket.remoteAddress + socket.remotePort
   const hashedKey = await bcrypt.hash(key, 1);
 
