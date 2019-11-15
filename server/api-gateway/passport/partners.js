24c70085c69ab6c8647ab706a6f1c66c57ae9d94
plm9606 <plm9606@naver.com>
2019-11-15 11:10:29 +0900

    fix(server/api-gateway): passport 전략 설정 옵션 추가
    localStrategy의 default 파라미터명 변경을 위한 옵션 추가.
    세션을 사용하지 않기 때문에 serializer, deserializer 삭제.

--- server/api-gateway/passport/partners.js
+++ server/api-gateway/passport/partners.js
@@ -3,21 +3,19 @@ const LocalStrategy = require("passport-local").Strategy;
 const Partner = require("../models/partner");
 
 passport.use(
-  new LocalStrategy((email, password, done) => {
-    Partner.findOne({ email: email, password: password }, (err, partner) => {
-      if (err) return done(err);
-      if (!partner) return done(null, false, { message: "로그인 실패" });
-      return done(null, partner);
-    });
-  })
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password"
+    },
+    (email, password, done) => {
+      Partner.findOne({ email: email, password: password }, (err, partner) => {
+        if (err) return done(err);
+        if (!partner) return done(null, false, { message: "로그인 실패" });
+        return done(null, partner);
+      });
+    }
+  )
 );
-
-passport.serializeUser((partner, done) => {
-  done(null, partner);
-});
-
-passport.deserializeUser((partner, done) => {
-  done(null, partner);
-});
 
 module.exports = passport;
