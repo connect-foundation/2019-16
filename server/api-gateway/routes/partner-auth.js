32adcd1059700cfe20f9182ea4ffb82ea276ae7d
plm9606 <plm9606@naver.com>
2019-11-15 11:05:26 +0900

    chore(server/api-gateway): 파트너 로그인 라우터에서 불필요한 테스트용 미들웨어 삭제
    테스트를 위해 passport 인증 미들웨어 다음 빈 비들웨어를 생성했었음
    이를 삭제함

--- server/api-gateway/routes/partner-auth.js
+++ server/api-gateway/routes/partner-auth.js
@@ -2,24 +2,18 @@ const Router = require("koa-router");
 const partnersRouter = new Router();
 const passportForPartners = require("../passport/partners");
 
-partnersRouter.post(
-  "/login",
-  async (ctx, next) => {
-    return passportForPartners.authenticate(
-      "local",
-      (err, user, info, status) => {
-        if (err) ctx.body = { msg: "시스템 에러" };
-        if (!user) ctx.body = { msg: info.message };
+partnersRouter.post("/login", async (ctx, next) => {
+  return passportForPartners.authenticate(
+    "local",
+    (err, user, info, status) => {
+      if (err) ctx.body = { msg: "시스템 에러" };
+      if (!user) ctx.body = { msg: info.message };
 
-        // jwt 토큰 생성
-        // 파트너 페이지로 redirection
-        next();
-      }
-    )(ctx);
-  },
-  ctx => {
-    debugger;
-  }
-);
+      // jwt 토큰 생성
+      // 파트너 페이지로 redirection
+      ctx.body = { msg: "로그인 성공", user: user };
+    }
+  )(ctx);
+});
 
 module.exports = partnersRouter;
