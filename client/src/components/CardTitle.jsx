d0a1e723ca664985dc4d2a1258135ede9df2c62a
김세진 <sejjj120@gmail.com>
2019-11-18 19:06:07 +0900

    feat(client): 스터디 카드의 타이틀 컴포넌트

---
+++ client/src/components/CardTitle.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h3``;
+
+const CardTitle = ({ title }) => <Title>{title}</Title>;
+
+export default CardTitle;
