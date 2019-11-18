a3a168ab2e6789be5efa63cdf3f6bc32add367db
김세진 <sejjj120@gmail.com>
2019-11-18 19:41:35 +0900

    test(client): mock 데이터 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import StudyGroupCard from "./components/StudyGroupCard";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">{/* <StudyGroupCard props={data} /> */}</div>;
 }
 
 export default App;
+
+// const data = {
+//   src:
+//     "https://nicholashowlett.com.au/wp-content/uploads/2016/05/coding-cat-768x512.jpg",
+//   alt: "img",
+//   title: "자바스크립트 기초 공부해요",
+//   location: "강남",
+//   time: "월수 | 20:00 ~ | 2시간",
+//   now: 8,
+//   max: 10
+// };
