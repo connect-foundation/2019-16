f8a43a05e549e28e489ff5c73aa4b364af50b166
김세진 <sejjj120@gmail.com>
2019-11-18 19:07:39 +0900

    feat(client): 스터디 카드의 시간 정보 컴포넌트
    요일, 시작시간, 진행시간 표시.

---
+++ client/src/components/StudyTime.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const Time = styled.div``;
+
+const StudyTime = ({ time }) => <Time>{time}</Time>;
+
+export default StudyTime;
