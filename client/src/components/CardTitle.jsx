cd0382e6e1d7d5ba05181d6993d11dff0b45bd46
Taehyun Lim <dlatns0201@naver.com>
2019-11-19 17:53:17 +0900

    style(cardtitle): 카드 제목 폰트 스타일 적용
    나눔바른고딕체 적용
    폰트체, 사이즈, 높이 변경

--- client/src/components/CardTitle.jsx
+++ client/src/components/CardTitle.jsx
@@ -2,7 +2,24 @@ import React from "react";
 import styled from "styled-components";
 
 const Title = styled.h3`
-  margin: 0.5rem;
+  @font-face {
+    font-family: "NanumBarunGothic";
+    font-style: normal;
+    font-weight: 400;
+    src: url("//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.eot");
+    src: url("//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.eot?#iefix")
+        format("embedded-opentype"),
+      url("//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.woff")
+        format("woff"),
+      url("//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.ttf")
+        format("truetype");
+  }
+  font-family: "NanumBarunGothic", sans-serif;
+  font-weight: bold;
+  line-height: 2.2rem;
+  font-size: 1.6rem;
+
+  margin: 1rem 0rem;
 `;
 
 const CardTitle = ({ title }) => <Title>{title}</Title>;
