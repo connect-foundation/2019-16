bf877e8618c27d6a71194a4afba54dfde5a1e06e
soob-forest <dltntnqo@naver.com>
2019-11-19 18:51:38 +0900

    feat(lib/redis): redis의 appList에 app 정보 추가

--- server/lib/redis/index.js
+++ server/lib/redis/index.js
@@ -1,2 +1,22 @@
 const redis = require("redis");
 const client = redis.createClient();
+
+exports.saveApp = async (appName, { host, port }) => {
+  let isAlready = 0;
+
+  await new Promise(res => {
+    client.exists(appName, (err, bool) => {
+      isAlready = bool;
+      res();
+    });
+  });
+  return new Promise(res => {
+    let resultBool = false;
+
+    if (isAlready === 0) {
+      client.hset(appName, "host", host, "port", port);
+      resultBool = true;
+    } else resultBool = false;
+    res(resultBool);
+  });
+};
