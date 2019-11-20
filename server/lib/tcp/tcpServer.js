4960280d69434a6a976e31829c8b48fc95ec63a5
soob-forest <dltntnqo@naver.com>
2019-11-20 14:36:24 +0900

    fix(tcpserver): tcpsever 생성자 인자에서 query제거
    app class와 tcpserver class를 분리하며 query에 대한 책임이 없어졌음

--- server/lib/tcp/tcpServer.js
+++ server/lib/tcp/tcpServer.js
@@ -3,54 +3,53 @@ const logger = require("../../services/logger/logger");
 const { makeKey, PACKET_SPLITTER } = require("./util");
 
 class TcpServer {
-  constructor(name, host, port, query) {
-    this.context = {
-      name,
-      host,
-      port,
-      query
-    };
+	constructor(name, host, port) {
+		this.context = {
+			name,
+			host,
+			port
+		};
 
-    this.dataMap = {};
-    this.server = net.createServer(socket => {
-      this.onCreate(socket);
+		this.dataMap = {};
+		this.server = net.createServer(socket => {
+			this.onCreate(socket);
 
-      socket.on("close", () => {
-        this.onClose(socket);
-      });
+			socket.on("close", () => {
+				this.onClose(socket);
+			});
 
-      socket.on("data", async data => {
-        const key = await makeKey(socket);
+			socket.on("data", async data => {
+				const key = await makeKey(socket);
 
-        let mergedPacket = !this.dataMap[key]
-          ? data.toString()
-          : this.dataMap[key].concat(data.toString());
+				let mergedPacket = !this.dataMap[key]
+					? data.toString()
+					: this.dataMap[key].concat(data.toString());
 
-        const packets = mergedPacket.split(PACKET_SPLITTER);
+				const packets = mergedPacket.split(PACKET_SPLITTER);
 
-        packets.forEach((packet, index) => {
-          if (
-            mergedPacket[mergedPacket.length - 1] !== PACKET_SPLITTER &&
-            index === packets.length - 1
-          ) {
-            this.dataMap[key] = packets[index];
-            return;
-          }
-          if (packet === "") return;
-          this.onRead(socket, JSON.parse(packet));
-        });
-      });
-    });
-    this.server.listen(port, host, () => {
-      logger.info(`${name} Server Listening!`);
-    });
-  }
-  onCreate(socket) {}
-  onClose() {}
-  onRead(socket, data) {
-    logger.info(data);
-    socket.write(data);
-  }
+				packets.forEach((packet, index) => {
+					if (
+						mergedPacket[mergedPacket.length - 1] !== PACKET_SPLITTER &&
+						index === packets.length - 1
+					) {
+						this.dataMap[key] = packets[index];
+						return;
+					}
+					if (packet === "") return;
+					this.onRead(socket, JSON.parse(packet));
+				});
+			});
+		});
+		this.server.listen(port, host, () => {
+			logger.info(`${name} Server Listening!`);
+		});
+	}
+	onCreate(socket) { }
+	onClose() { }
+	onRead(socket, data) {
+		logger.info(data);
+		socket.write(data);
+	}
 }
 
 module.exports = TcpServer;
