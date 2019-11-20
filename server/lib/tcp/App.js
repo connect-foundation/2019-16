c96d782fd6927bfac64a6f3b864840fd846ad0f3
soob-forest <dltntnqo@naver.com>
2019-11-20 14:38:40 +0900

    fix(tcp/app): query를 배열로 변경
    app은 여러개의 query를 가지고 있어야 된다.

--- server/lib/tcp/App.js
+++ server/lib/tcp/App.js
@@ -3,8 +3,9 @@ const TcpClient = require("./TcpClient");
 const logger = require("../../services/logger/logger");
 
 class App extends TcpServer {
-  constructor(name, host, port, query) {
-    super(name, host, port, query);
+  constructor(name, host, port, query = []) {
+    super(name, host, port);
+    this.query = query;
     this.isConnectToAppListManager = false;
   }
 
@@ -13,7 +14,7 @@ class App extends TcpServer {
       "127.0.0.1",
       8100,
       () => {
-        this.isConnectToDistributor = true;
+        this.isConnectToAppListManager = true;
         logger.info(
           `${this.context.host}:${this.context.port} is connected to app list manager`
         );
