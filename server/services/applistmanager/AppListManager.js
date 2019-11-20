a5cd66c2b31dcb415209f253ea9cc024430774a5
soob-forest <dltntnqo@naver.com>
2019-11-20 17:40:03 +0900

    feat(applistmanager): onRead 이벤트 함수 구현
    AddListManager 서버로 데이터가 오면 응답해주기 위한 함수

--- server/services/applistmanager/AppListManager.js
+++ server/services/applistmanager/AppListManager.js
@@ -1,9 +1,60 @@
-const TcpServer = require("./TcpServer");
+const TcpServer = require("../../lib/tcp/tcpServer");
+const { makePacket, makeKey } = require("../../lib/tcp/util");
+const logger = require("../logger/logger");
+const { setAppbypName, deletebypName, updatdAppbypName, getAppbypName, getAllApps } = require("../../lib/redis")
+
+let appManagerInstance;
 
 class AppListManager extends TcpServer {
-  constructor(name, host, port, query) {
-    super(name, host, port, query);
+  constructor(name, host, port, query = []) {
+    if (appManagerInstance) return appManagerInstance;
+    super(name, host, port);
+    this.query = query;
+    appManagerInstance = this;
   }
+  async onRead(socket, data) {
+
+    const { method, query, info } = data;
+    const key = makeKey(socket);
+    let result;
+
+    try {
+      if (method === "POST") {
+        switch (query) {
+          case "add":
+            result = await setAppbypName(key, info);
+            break;
+          case "delete":
+            result = await deletebypName(key, info);
+            break;
+          case "update":
+            result = await updatdAppbypName(key, info);
+            break;
+          default:
+            break;
+        }
+      }
+      if (method === "GET") {
+        switch (query) {
+          case "get":
+            result = await getAppbypName(key);
+            break;
+          case "getAll":
+            result = await getAllApps();
+            break;
+          default:
+            break;
+        }
+        const packet = makePacket("REPLY", "apps", {}, { apps: result }, this.context);
+
+        this.send(socket, packet)
+      }
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
 }
 
 module.exports = AppListManager;
