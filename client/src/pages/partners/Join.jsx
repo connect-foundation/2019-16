a11f4b51ed41493878930181c2a22482a311a7c4
김세진 <sejjj120@gmail.com>
2019-11-21 02:07:51 +0900

    feat(client/partners): 파트너 가입 페이지

---
+++ client/src/pages/partners/Join.jsx
@@ -0,0 +1,81 @@
+import React, { useReducer } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const JoinForm = styled.div`
+  width: 50%;
+  margin: 10rem auto;
+`;
+
+const PartnersJoinPage = () => {
+  return (
+    <JoinForm>
+      <h1 class="title is-1">Partner Join</h1>
+      <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="이름" />
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Email</label>
+        <div class="control has-icons-left has-icons-right">
+          <input
+            class="input is-danger"
+            type="email"
+            placeholder="example@example.com"
+          />
+          <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+          </span>
+          <span class="icon is-small is-right">
+            <i class="fas fa-exclamation-triangle"></i>
+          </span>
+        </div>
+        <p class="help is-danger">This email is invalid</p>
+      </div>
+
+      <div class="field">
+        <label class="label">Password</label>
+        <p class="control has-icons-left">
+          <input class="input" type="password" placeholder="비밀번호" />
+          <span class="icon is-small is-left">
+            <i class="fas fa-lock"></i>
+          </span>
+        </p>
+      </div>
+
+      <div class="field">
+        <p class="control has-icons-left">
+          <input class="input" type="password" placeholder="비밀번호 확인" />
+          <span class="icon is-small is-left">
+            <i class="fas fa-lock"></i>
+          </span>
+        </p>
+      </div>
+
+      <div class="field">
+        <label class="label">CID</label>
+        <div class="control">
+          <input
+            class="input"
+            type="text"
+            placeholder="가맹점 코드(10자)"
+            maxLength="10"
+            style={{ "text-transform": "uppercase" }}
+          />
+        </div>
+      </div>
+
+      <button class="button is-danger is-medium is-rounded is-fullwidth">
+        가입하기
+      </button>
+      <Link to="/partners/login">
+        <button class="button is-text is-small">로그인으로 돌아가기</button>
+      </Link>
+    </JoinForm>
+  );
+};
+
+export default PartnersJoinPage;
