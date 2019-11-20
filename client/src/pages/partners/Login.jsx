0a812bbed0b3348e3aa20692a50b9d37a416821f
김세진 <sejjj120@gmail.com>
2019-11-21 02:08:23 +0900

    feat(client/partners): 파트너 로그인 페이지

---
+++ client/src/pages/partners/Login.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const LoginForm = styled.div`
+  width: 50%;
+  margin: 10rem auto;
+`;
+
+const PartnersLoginPage = () => (
+  <LoginForm>
+    <h1 className="title is-1">Partner Login</h1>
+
+    <div className="field">
+      <p className="control has-icons-left has-icons-right">
+        <input className="input" type="email" placeholder="Email" />
+        <span className="icon is-small is-left">
+          <i className="fas fa-envelope"></i>
+        </span>
+        <span className="icon is-small is-right">
+          <i className="fas fa-check"></i>
+        </span>
+      </p>
+    </div>
+
+    <div className="field">
+      <p className="control has-icons-left">
+        <input className="input" type="password" placeholder="Password" />
+        <span className="icon is-small is-left">
+          <i className="fas fa-lock"></i>
+        </span>
+      </p>
+    </div>
+
+    <button className="button is-danger is-rounded is-fullwidth is-medium">
+      Login
+    </button>
+
+    <Link to="/partners/join">
+      <button class="button is-text is-small">파트너 등록하기</button>
+    </Link>
+  </LoginForm>
+);
+
+export default PartnersLoginPage;
