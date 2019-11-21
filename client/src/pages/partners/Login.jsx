e4d493c22a6288201eddb8f3cc514f07560bbbe5
김세진 <sejjj120@gmail.com>
2019-11-21 14:27:29 +0900

    feat(client/partners): 제출 버튼 컴포넌트 작성
    파트너 로그인 페이지와 가입 페이지에서 isLoading 상태를 확인하여 버튼 모습을 변경한다.

--- client/src/pages/partners/Login.jsx
+++ client/src/pages/partners/Login.jsx
@@ -1,45 +1,64 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import SubmitButton from "../../components/common/SubmitButton";
 
 const LoginForm = styled.div`
   width: 50%;
   margin: 10rem auto;
 `;
 
-const PartnersLoginPage = () => (
-  <LoginForm>
-    <h1 className="title is-1">Partner Login</h1>
+const reducer = (state, action) => {};
 
-    <div className="field">
-      <p className="control has-icons-left has-icons-right">
-        <input className="input" type="email" placeholder="Email" />
-        <span className="icon is-small is-left">
-          <i className="fas fa-envelope"></i>
-        </span>
-        <span className="icon is-small is-right">
-          <i className="fas fa-check"></i>
-        </span>
-      </p>
-    </div>
+const PartnersLoginPage = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    email: "",
+    password: "",
+    isLoading: false
+  });
 
-    <div className="field">
-      <p className="control has-icons-left">
-        <input className="input" type="password" placeholder="Password" />
-        <span className="icon is-small is-left">
-          <i className="fas fa-lock"></i>
-        </span>
-      </p>
-    </div>
+  return (
+    <LoginForm>
+      <h1 className="title is-1">Partner Login</h1>
 
-    <button className="button is-danger is-rounded is-fullwidth is-medium">
-      Login
-    </button>
+      <div className="field">
+        <p className="control has-icons-left has-icons-right">
+          <input
+            className="input"
+            type="email"
+            placeholder="Email"
+            // onChange={}
+          />
+          <span className="icon is-small is-left">
+            <i className="fas fa-envelope"></i>
+          </span>
+          <span className="icon is-small is-right">
+            <i className="fas fa-check"></i>
+          </span>
+        </p>
+      </div>
 
-    <Link to="/partners/join">
-      <button class="button is-text is-small">파트너 등록하기</button>
-    </Link>
-  </LoginForm>
-);
+      <div className="field">
+        <p className="control has-icons-left">
+          <input
+            className="input"
+            type="password"
+            placeholder="Password"
+            // onChange={}
+          />
+          <span className="icon is-small is-left">
+            <i className="fas fa-lock"></i>
+          </span>
+        </p>
+      </div>
+
+      <SubmitButton isLoading={state.isLoading} content="로그인" />
+
+      <Link to="/partners/join">
+        <button class="button is-text is-small">파트너 등록하기</button>
+      </Link>
+    </LoginForm>
+  );
+};
 
 export default PartnersLoginPage;
