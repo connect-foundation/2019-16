5fdc3262570c9fef81cc0a78e4210c0382c8ae7a
soob-forest <dltntnqo@naver.com>
2019-11-21 21:47:39 +0900

    feat(search): 검색에 관한 query 함수 추가
    searchStudyGroup 라는 query로 요청이 오면 응답하는 함수

--- server/services/search/Search.js
+++ server/services/search/Search.js
@@ -9,6 +9,35 @@ const { Client } = require('@elastic/elasticsearch')
 const client = new Client({ node: `http://${ELASTIC_HOST}:${ELASTIC_PORT}` })
 
 
+async function searchStudyGroup(info) {
+  const { searchWord, category, status, tags } = info;
+
+
+  const { body } = await client.search({
+    index: 'studygroup',
+    body: {
+      query: {
+        bool: {
+          must: [{
+            match: {
+              title: searchWord
+            }
+          }],
+          filter: {
+            term: {
+              isRecruit: true
+            }
+          }
+        }
+      }
+    }
+  })
+
+  return body.hits.hits;
+}
+const queryMap = {
+  searchStudyGroup: searchStudyGroup,
+}
 
 class Search extends App {
   constructor(name, host, port) {
