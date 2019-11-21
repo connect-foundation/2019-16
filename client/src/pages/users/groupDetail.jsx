8c70f412f708a9573d4d78cde1aade56fe3dc144
Taehyun Lim <dlatns0201@naver.com>
2019-11-21 18:48:25 +0900

    test(group_detail): test 실행 준비
    group detail reducer에 초기 state 값 지정

--- client/src/pages/users/groupDetail.jsx
+++ client/src/pages/users/groupDetail.jsx
@@ -1,9 +1,12 @@
-import React, { useReducer } from "react";
+import React, { memo, useReducer } from "react";
 import styled from "styled-components";
 import Header from "../../components/groupDetail/Header";
 import Main from "../../components/groupDetail/Main";
 import Intro from "../../components/groupDetail/Intro";
-import { groupDetail as groupDetailReducer } from "../../reducer/groupDetail";
+import {
+  groupDetail as groupDetailReducer,
+  initialState
+} from "../../reducer/groupDetail";
 
 const StyledGroupDetail = styled.div`
   display: flex;
@@ -14,7 +17,7 @@ const StyledGroupDetail = styled.div`
 `;
 
 const GroupDetail = () => {
-  const [state, dispatch] = useReducer(groupDetailReducer, {});
+  const [state, dispatch] = useReducer(groupDetailReducer, initialState);
 
   return (
     <StyledGroupDetail>
