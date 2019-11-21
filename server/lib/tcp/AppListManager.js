795e5ea08ae0d70609b67835a8c5f234ee203538
soob-forest <dltntnqo@naver.com>
2019-11-21 11:03:22 +0900

    fix: lib 폴더의 applistmanager 삭제

--- server/lib/tcp/AppListManager.js
+++
@@ -1,9 +0,0 @@
-const TcpServer = require("./TcpServer");
-
-class AppListManager extends TcpServer {
-  constructor(name, host, port, query) {
-    super(name, host, port, query);
-  }
-}
-
-module.exports = AppListManager;
