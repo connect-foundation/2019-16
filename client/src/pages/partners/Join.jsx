aa849c07524b665d648d4c8b8db2442998b8d52a
김세진 <sejjj120@gmail.com>
2019-11-21 15:50:34 +0900

    feat(client): email 입력창 컴포넌트화
    파트너 로그인과 가입에 사용되는 email 컴포넌트 생성.
    components/common에 저장.

--- client/src/pages/partners/Join.jsx
+++ client/src/pages/partners/Join.jsx
@@ -1,12 +1,15 @@
 import React, { useReducer } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import EmailInput from "../../components/common/EmailInput";
 import SubmitButton from "../../components/common/SubmitButton";
 
 const JoinForm = styled.div`
   width: 50%;
   margin: 10rem auto;
 `;
+
+const EMAIL_WARN_MSG = "이미 존재하는 이메일입니다.";
 
 const reducer = () => {};
 
@@ -16,12 +19,14 @@ const PartnersJoinPage = () => {
     email: "",
     password: "",
     CID: "",
-    isLoading: false
+    isLoading: false,
+    emailWarning: false
   });
 
   return (
     <JoinForm>
       <h1 class="title is-1">Partner Join</h1>
+
       <div class="field">
         <label class="label">Name</label>
         <div class="control">
@@ -29,23 +34,7 @@ const PartnersJoinPage = () => {
         </div>
       </div>
 
-      <div class="field">
-        <label class="label">Email</label>
-        <div class="control has-icons-left has-icons-right">
-          <input
-            class="input is-danger"
-            type="email"
-            placeholder="example@example.com"
-          />
-          <span class="icon is-small is-left">
-            <i class="fas fa-envelope"></i>
-          </span>
-          <span class="icon is-small is-right">
-            <i class="fas fa-exclamation-triangle"></i>
-          </span>
-        </div>
-        <p class="help is-danger">This email is invalid</p>
-      </div>
+      <EmailInput emailWarning={state.emailWarning} warnMsg={EMAIL_WARN_MSG} />
 
       <div class="field">
         <label class="label">Password</label>
@@ -80,7 +69,7 @@ const PartnersJoinPage = () => {
       </div>
 
       <SubmitButton isLoading={state.isLoading} content="가입하기" />
-
+      <br />
       <Link to="/partners/login">
         <button class="button is-text is-small">로그인으로 돌아가기</button>
       </Link>
--- client/src/pages/partners/Login.jsx
+++ client/src/pages/partners/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import EmailInput from "../../components/common/EmailInput";
 import SubmitButton from "../../components/common/SubmitButton";
 
 const LoginForm = styled.div`
@@ -8,44 +9,27 @@ const LoginForm = styled.div`
   margin: 10rem auto;
 `;
 
+const EMAIL_WARN_MSG = "존재하지 않는 이메일 혹은 잘못된 비밀번호입니다.";
+
 const reducer = (state, action) => {};
 
 const PartnersLoginPage = () => {
   const [state, dispatch] = useReducer(reducer, {
     email: "",
     password: "",
-    isLoading: false
+    isLoading: false,
+    emailWarning: false
   });
 
   return (
     <LoginForm>
       <h1 className="title is-1">Partner Login</h1>
 
+      <EmailInput emailWarning={state.emailWarning} warnMsg={EMAIL_WARN_MSG} />
       <div className="field">
-        <p className="control has-icons-left has-icons-right">
-          <input
-            className="input"
-            type="email"
-            placeholder="Email"
-            // onChange={}
-          />
-          <span className="icon is-small is-left">
-            <i className="fas fa-envelope"></i>
-          </span>
-          <span className="icon is-small is-right">
-            <i className="fas fa-check"></i>
-          </span>
-        </p>
-      </div>
-
-      <div className="field">
+        <label class="label">Password</label>
         <p className="control has-icons-left">
-          <input
-            className="input"
-            type="password"
-            placeholder="Password"
-            // onChange={}
-          />
+          <input className="input" type="password" placeholder="비밀번호" />
           <span className="icon is-small is-left">
             <i className="fas fa-lock"></i>
           </span>
@@ -53,7 +37,7 @@ const PartnersLoginPage = () => {
       </div>
 
       <SubmitButton isLoading={state.isLoading} content="로그인" />
-
+      <br />
       <Link to="/partners/join">
         <button class="button is-text is-small">파트너 등록하기</button>
       </Link>
