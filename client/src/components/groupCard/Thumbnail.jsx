67affe21c643782c4198b3137e3b975b5e304780
plm9606 <plm9606@naver.com>
2019-11-22 11:39:26 +0900

    chore(client/main_page): 스터디그룹 카드 사진 크기 변경
    사진의 크기를 카드 높이의 50%로 설정함

--- client/src/components/groupCard/Thumbnail.jsx
+++ client/src/components/groupCard/Thumbnail.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 const Thumbnail = styled.div`
+  height: 50%;
+  overflow: hidden;
   img {
     width: 100%;
-    height: 12rem;
+    height: 100%;
     border-top-left-radius: 0.2rem;
     border-top-right-radius: 0.2rem;
   }
