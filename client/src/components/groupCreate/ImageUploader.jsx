bc3a41a73f5e94642f3a8269cfbb4b5d42233515
Taehyun Lim <dlatns0201@naver.com>
2019-11-24 18:07:26 +0900

    feat(group_create_image_uploader): 이미지 업로더 컴포넌트 작성

--- client/src/components/groupCreate/ImageUploader.jsx
+++ client/src/components/groupCreate/ImageUploader.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import styled from "styled-components";
 
-const StyledImageUploader = styled.div``;
+const StyledImageUploader = styled.div`
+  .image {
+    width: 16rem;
+    height: 12rem;
+    border: 1px solid gray;
+    margin-bottom: 1rem;
+  }
+`;
 
 const ImageUploader = () => {
-  return <StyledImageUploader></StyledImageUploader>;
+  const [imageInfo, setImageInfo] = useState({
+    file: null,
+    imagePreviewUrl: null
+  });
+
+  const onChange = useCallback(e => {
+    e.preventDefault();
+
+    const reader = new FileReader();
+    const file = e.target.files[0];
+
+    reader.onloadend = () => {
+      setImageInfo({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    };
+
+    reader.readAsDataURL(file);
+  }, []);
+
+  const { imagePreviewUrl } = imageInfo;
+  return (
+    <StyledImageUploader>
+      {(imageInfo.file && <img src={imagePreviewUrl} className="image" />) || (
+        <img className="image" />
+      )}
+      <input type="file" onChange={onChange} />
+    </StyledImageUploader>
+  );
 };
 
 export default ImageUploader;
