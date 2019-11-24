8a5517617d318503539cb25d5fe5e88eff5e44c5
Taehyun Lim <dlatns0201@naver.com>
2019-11-24 15:57:01 +0900

    feat(group_create): 주 카테고리 선택 기능 추가
    테스팅을 위해 임시 데이터 작성
    useState로 주 카테고리 상태 작성

--- client/src/pages/users/groupCreate.jsx
+++ client/src/pages/users/groupCreate.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import styled from "styled-components";
 
-const StyledGroupCreate = styled.div``;
+const StyledGroupCreate = styled.div`
+  margin: 2rem 0;
+  .category {
+    cursor: pointer;
+  }
+`;
+const primaryCategories = ["프로그래밍", "자격증", "외국어", "면접", "지역"];
+const secondaryCategories = {
+  프로그래밍: ["C++", "Java", "JavaScript"],
+  자격증: ["IT", "운전", "보건", "식품"],
+  외국어: ["영어", "중국어", "불어", "스페인어"],
+  면접: ["공채", "상시채용", "특채", "기술면접", "임원면접"],
+  지역: [
+    "경기도",
+    "서울",
+    "울산광역시",
+    "인천광역시",
+    "광주광역시",
+    "부산광역시"
+  ]
+};
 
 const GroupCreate = props => {
-  return <StyledGroupCreate></StyledGroupCreate>;
+  const [primaryCategory, setPrimaryCategory] = useState(null);
+  const primaryCategoryEvent = useCallback(e => {
+    const categoryName = e.target.getAttribute("name");
+    setPrimaryCategory(categoryName);
+  }, []);
+
+  return (
+    <StyledGroupCreate>
+      <div class="breadcrumb is-centered" aria-label="breadcrumbs">
+        <ul>
+          {primaryCategories.map(category => (
+            <li
+              key={category}
+              name={category}
+              className="category has-text-primary is-size-5"
+              onClick={primaryCategoryEvent}
+            >
+              {category}
+            </li>
+          ))}
+        </ul>
+        )}
+      </div>
+    </StyledGroupCreate>
+  );
 };
 
 export default GroupCreate;
