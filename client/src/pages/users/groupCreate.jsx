fa6a89c3862bec8b400a84551f4327f6b8f4772f
Taehyun Lim <dlatns0201@naver.com>
2019-11-24 21:20:27 +0900

    feat(group_create_daybtn): 요일 버튼 상태 저장 및 스타일 변경
    요일 취소 버튼 클릭 시 다른 작업을 하지 않을 때 focus가 남는 현상을 blur로 처리

--- client/src/pages/users/groupCreate.jsx
+++ client/src/pages/users/groupCreate.jsx
@@ -1,5 +1,6 @@
-import React, { useCallback, useState, useReducer } from "react";
+import React, { useCallback, useState, useReducer, useEffect } from "react";
 import styled from "styled-components";
+import classnames from "classnames";
 import Category from "../../components/groupCreate/Category";
 import ImageUploader from "../../components/groupCreate/ImageUploader";
 import TagInput from "../../components/groupCreate/TagInput";
@@ -33,14 +34,39 @@ const StyledGroupCreate = styled.div`
       height: auto;
     }
   }
+
+  .button:focus {
+    background-color: white;
+  }
 `;
 
-const days = ["일", "월", "화", "수", "목", "금", "토", "일"];
-
+const days = ["일", "월", "화", "수", "목", "금", "토"];
+const conditions = [];
+for (let i = 0; i < 7; i++) {
+  conditions[i] = {
+    isSelected: false,
+    class: classnames({ "is-focused": false })
+  };
+}
 const GroupCreate = props => {
   const [state, dispatch] = useReducer(groupCreateReducer, initialState);
   const { primary, secondary, primaryCategories, secondaryCategories } = state;
   const [tags, setTags] = useState([]);
+  const [dayCondition, setDayCondition] = useState(conditions);
+
+  const onClickDay = i => {
+    return e => {
+      e.target.blur();
+      const copyConditions = [...dayCondition];
+      copyConditions[i] = {
+        isSelected: !copyConditions[i].isSelected,
+        class: classnames({
+          "is-focused": !copyConditions[i].isSelected
+        })
+      };
+      setDayCondition(copyConditions);
+    };
+  };
 
   // title subtitle 최소 인원 최대 인원 그룹 소개 썸네일 위치 태그 날짜
   return (
@@ -70,11 +96,19 @@ const GroupCreate = props => {
       <TagInput tags={tags} setTags={setTags} />
 
       <div className="field has-addons">
-        {days.map(day => (
-          <p className="control">
-            <button className="button is-info is-outlined">{day}</button>
-          </p>
-        ))}
+        {days.map((day, idx) => {
+          return (
+            <p className="control" key={idx}>
+              <button
+                className={`button is-info is-outlined ${dayCondition[idx] &&
+                  dayCondition[idx].class}`}
+                onClick={onClickDay(idx)}
+              >
+                {day}
+              </button>
+            </p>
+          );
+        })}
       </div>
     </StyledGroupCreate>
   );
