722ebec3e9ffb964030258b81112d16377dba370
soob-forest <dltntnqo@naver.com>
2019-11-24 18:03:58 +0900

    test(search/index): 테스트를 위한 수정

--- server/services/search/index.js
+++ server/services/search/index.js
@@ -2,3 +2,7 @@ const Search = require("./Search");
 
 const search = new Search("search", "127.0.0.1", 8002);
 search.connectToAppListManager();
+
+
+const search2 = new Search("search2", "127.0.0.1", 8003);
+search2.connectToAppListManager();
