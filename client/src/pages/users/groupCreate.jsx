5341edc542d50338398567094f290c3f5c529946
Taehyun Lim <dlatns0201@naver.com>
2019-11-24 14:55:52 +0900

    feat(group_create): 그룹 생성 페이지 파일 생성

---
+++ client/src/pages/users/groupCreate.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledGroupCreate = styled.div``;
+
+const GroupCreate = props => {
+  return <StyledGroupCreate></StyledGroupCreate>;
+};
+
+export default GroupCreate;
