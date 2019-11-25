a6189d3194b62f49ea9026dfdff7bff44f1d03cf
soob-forest <>
2019-11-25 19:13:14 +0900

    fix:  makekey 부분 미들웨어로 뺌

--- server/api-gateway/routes/search.js
+++ server/api-gateway/routes/search.js
@@ -1,40 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-const { makePacket, makeKey } = require("../../lib/tcp/util");
+const { makePacket } = require("../../lib/tcp/util");
 
 module.exports = function (apigateway) {
 
-    router.get('/', async (req, res) => {
-        res.send("hello search")
-    })
-
-    router.get('/all', async (req, res) => {
-        const key = await makeKey(req.client);
-        const packet = makePacket("POST", "searchAllStudyGroupWithFiltering", { isRecruit: true }, {}, key, apigateway.context);
-
-        apigateway.resMap[key] = res;
-        apigateway.appClientMap["search"].write(packet);
-    })
-
-    router.get('/all2', async (req, res) => {
-        const key = await makeKey(req.client);
-        const packet = makePacket("POST", "searchAllStudyGroupWithFiltering2", { isRecruit: true }, {}, key, apigateway.context);
-
-        apigateway.resMap[key] = res;
-        apigateway.appClientMap["search2"].write(packet);
-    });
-
-    router.get('/:searchWord', async (req, res) => {
+    router.get('/query/:searchWord', async (req, res, next) => {
 
         const searchWord = req.params.searchWord;
-        const key = await makeKey(req.client);
-        const packet = makePacket("POST", "searchStudyGroup", { searchWord, isRecruit: true }, {}, key, apigateway.context);
 
-        apigateway.resMap[key] = res;
-        apigateway.appClientMap["search"].write(packet);
+        const packet = makePacket("POST", "searchStudyGroup", { searchWord, isRecruit: true }, {}, req.resKey, apigateway.context);
+
+        req.packet = packet;
+        next();
     })
 
+    router.get('/all', async (req, res, next) => {
+
+        const packet = makePacket("POST", "searchAllStudyGroupWithFiltering", { isRecruit: true }, {}, req.resKey, apigateway.context);
+
+        req.packet = packet;
+        next();
+
+    })
     return router;
 }
 
