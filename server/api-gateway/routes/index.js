fdaf6598db8f01871be873c2845def68f984f7ca
김세진 <sejjj120@gmail.com>
2019-11-25 20:04:25 +0900

    chore: 불필요한 파일 삭제

--- server/api-gateway/routes/index.js
+++ server/api-gateway/routes/index.js
@@ -3,6 +3,5 @@ const authRouter = new Router();
 const partnersRouter = require("./partner-auth");
 
 authRouter.use("/partners", partnersRouter.routes());
-// authRouter.use("/users");
 
 module.exports = authRouter;
