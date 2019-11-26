cb5a0f36153021411eedb902aba51bfff6cdaeb1
김세진 <sejjj120@gmail.com>
2019-11-26 19:12:56 +0900

    feat(gateway/auth): 파트너 회원가입 미들웨어
    password를 암호화 한 후에 데이터베이스에 저장.
    에러 발생시 가입 페이지로 리다이렉트.
    에러가 없다면 로그인 페이지로 리다이렉트.

---
+++ server/api-gateway/middleware/partner-join.js
@@ -0,0 +1,22 @@
+const Partner = require("../models/partner");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+module.exports = (req, res) => {
+  const { name, email, password, cid } = req.body;
+
+  bcrypt.hash(password, saltRounds, async (err, hash) => {
+    Partner.create(
+      {
+        name: name,
+        email: email,
+        password: hash,
+        cid: cid
+      },
+      mongoError => {
+        if (mongoError) return res.redirect("/partners/join");
+        else return res.redirect("/partners/login");
+      }
+    );
+  });
+};
