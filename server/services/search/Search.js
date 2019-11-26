de9f8a56a504bb3874dacc43157dce9d1d034364
soob-forest <>
2019-11-26 15:00:18 +0900

    feat(search): elastic모듈 다른 파일로 이동

--- server/services/search/Search.js
+++ server/services/search/Search.js
@@ -1,71 +1,7 @@
-require("dotenv").config({ path: ".env.search" });
-const {
-  ELASTIC_HOST,
-  ELASTIC_PORT
-} = process.env;
-
 const App = require("../../lib/tcp/App");
-const { Client } = require('@elastic/elasticsearch')
-const client = new Client({ node: `http://${ELASTIC_HOST}:${ELASTIC_PORT}` })
 const { makePacket } = require("../../lib/tcp/util");
+const { searchAllStudyGroupWithFiltering, searchStudyGroup, bulkStudyGroups } = require("./elasticsearch")
 
-
-async function searchStudyGroup(info) {
-  const { searchWord, category, isRecruit, tags } = info;
-
-  const { body } = await client.search({
-    index: 'studygroup',
-    body: {
-      query: {
-        bool: {
-          must: [{
-            match: {
-              title: searchWord
-            }
-          }],
-          filter: {
-            term: {
-              isRecruit: isRecruit
-            }
-          }
-        }
-      }
-    }
-  })
-
-  const result = body.hits.hits.map((hit) => {
-    return hit._source;
-  })
-
-  return result;
-}
-async function searchAllStudyGroupWithFiltering(info) {
-  const { category, isRecruit, tags } = info;
-
-
-  const { body } = await client.search({
-    index: 'studygroup',
-    body: {
-      query: {
-        bool: {
-          must: [{
-            match_all: {}
-          }],
-          filter: {
-            term: {
-              isRecruit: isRecruit
-            }
-          }
-        }
-      }
-    }
-  })
-  const result = body.hits.hits.map((hit) => {
-    return hit._source;
-  })
-
-  return result;
-}
 const queryMap = {
   searchStudyGroup: searchStudyGroup,
   searchAllStudyGroupWithFiltering: searchAllStudyGroupWithFiltering,
