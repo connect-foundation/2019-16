e65145fa1108b0e59b66a281e9ae9606a80b07cf
김세진 <sejjj120@gmail.com>
2019-11-26 17:42:04 +0900

    style(gateway): prettier 적용

--- server/api-gateway/index.js
+++ server/api-gateway/index.js
@@ -6,13 +6,13 @@ const express = require("express");
 const server = express();
 const authRouter = require("./routes/auth");
 const {
-    GATE_EXPRESS_PORT,
-    GATE_TCP_PORT,
-    GATE_NAME,
-    PARTNERS_MONGO_URI,
-    PARTNERS_USER,
-    PARTNERS_PASS,
-    GATE_HOST
+  GATE_EXPRESS_PORT,
+  GATE_TCP_PORT,
+  GATE_NAME,
+  PARTNERS_MONGO_URI,
+  PARTNERS_USER,
+  PARTNERS_PASS,
+  GATE_HOST
 } = process.env;
 
 mongoose
@@ -28,91 +28,86 @@ mongoose
   });
 
 class ApiGateway extends App {
-    constructor() {
-        super(GATE_NAME, GATE_HOST, GATE_TCP_PORT);
-        this.appClientMap = {};
-        this.icConnectMap = {};
-        this.resMap = {};
-    }
+  constructor() {
+    super(GATE_NAME, GATE_HOST, GATE_TCP_PORT);
+    this.appClientMap = {};
+    this.icConnectMap = {};
+    this.resMap = {};
+  }
 }
 const apigateway = new ApiGateway();
 const searchRouter = require("./routes/search")(apigateway);
 
 async function setResponseKey(req, res, next) {
-    const key = await makeKey(req.client);
+  const key = await makeKey(req.client);
 
-    req.resKey = key;
-    apigateway.resMap[key] = res;
-
-    next();
+  req.resKey = key;
+  apigateway.resMap[key] = res;
+  next();
 }
 
 function writePacket(req, res, next) {
-    const appName = req.path.split("/")[2];
+  const appName = req.path.split("/")[2];
 
-    apigateway.appClientMap[appName].write(req.packet);
+  apigateway.appClientMap[appName].write(req.packet);
 }
-
-
-
-let searchRouter = require("./routes/search")(apigateway)
-
-server.use(setResponseKey)
-
-server.get('/', (req, res) => res.send('Hello World!'));
-
-server.use('/api/search', searchRouter);
-
-server.use(writePacket)
-
-server.listen(GATE_EXPRESS_PORT, async () => {
-    connectToAllApps();
-})
 
 async function makeAppClient(name) {
-    try {
-        const client = await apigateway.connectToApp(name,
-            () => {
-                // connect이벤트 함수
-                apigateway.appClientMap[name] = client;
-                apigateway.icConnectMap[name] = true;
-                console.log(`${name} service connect`)
-            },
-            (data) => {
-                // data이벤트 함수
-                apigateway.resMap[data.key].json(data.body.studygroups);
-                delete apigateway.resMap[data.key];
-            },
-            () => {
-                apigateway.icConnectMap[name] = false;
-                console.log(`${name} service end`)
-            },
-            () => {
-                apigateway.icConnectMap[name] = false;
-                console.log(`${name} service error`)
-            }
-        );
+  try {
+    const client = await apigateway.connectToApp(
+      name,
+      () => {
+        // connect이벤트 함수
+        apigateway.appClientMap[name] = client;
+        apigateway.icConnectMap[name] = true;
+        console.log(`${name} service connect`);
+      },
+      data => {
+        // data이벤트 함수
+        apigateway.resMap[data.key].json(data.body.studygroups);
+        delete apigateway.resMap[data.key];
+      },
+      () => {
+        apigateway.icConnectMap[name] = false;
+        console.log(`${name} service end`);
+      },
+      () => {
+        apigateway.icConnectMap[name] = false;
+        console.log(`${name} service error`);
+      }
+    );
 
-        setInterval(() => {
-            if (!apigateway.icConnectMap[name]) {
-                console.log(`try connect to search2`);
-                client.connect();
-            }
-        }, 2000);
-        return client;
-    } catch (e) {
-        console.log(e)
-    }
+    setInterval(() => {
+      if (!apigateway.icConnectMap[name]) {
+        console.log(`try connect to search2`);
+        client.connect();
+      }
+    }, 2000);
+    return client;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
+/**
+ * 연결 가능한 모든 서비스들에 대한 tcp 클라이언트 생성
+ */
 async function connectToAllApps() {
-    const apps = await apigateway.getAllApps();
+  const apps = await apigateway.getAllApps();
 
-    const appNames = apps.map(app => app.name);
+  const appNames = apps.map(app => app.name);
 
-    appNames.forEach((appName) => {
-        makeAppClient(appName);
-    })
+  appNames.forEach(appName => {
+    makeAppClient(appName);
+  });
 }
 
+server.use(express.json());
+server.use(setResponseKey);
+server.use("/auth", authRouter);
+server.use("/api/search", searchRouter);
+server.use(writePacket);
 
+server.listen(GATE_EXPRESS_PORT || 8000, async () => {
+  connectToAllApps();
+});
