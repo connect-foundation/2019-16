0f092417ef2ba2bb3197bf6132fd5cdc692a4569
soob-forest <>
2019-11-27 21:58:31 +0900

    feat: cors, favicon, express.json 추가

--- server/api-gateway/index.js
+++ server/api-gateway/index.js
@@ -1,9 +1,12 @@
 require("dotenv").config({ path: ".env.gateway" });
+const path = require("path");
 const mongoose = require("mongoose");
 const App = require("../lib/tcp/App");
 const { makeKey } = require("../lib/tcp/util");
+const cors = require("cors");
 const express = require("express");
 const server = express();
+const favicon = require("express-favicon");
 const authRouter = require("./routes/auth");
 const {
   GATE_EXPRESS_PORT,
@@ -36,7 +39,6 @@ class ApiGateway extends App {
   }
 }
 const apigateway = new ApiGateway();
-const searchRouter = require("./routes/search")(apigateway);
 
 async function setResponseKey(req, res, next) {
   const key = await makeKey(req.client);
@@ -87,91 +89,96 @@ async function makeAppClient(name) {
   } catch (e) {
     console.log(e);
   }
-    if (req.path !== "/favicon.ico") {
-        const key = await makeKey(req.client);
+  if (req.path !== "/favicon.ico") {
+    const key = await makeKey(req.client);
 
-        req.resKey = key;
-        apigateway.resMap[key] = res;
-    }
-    next();
+    req.resKey = key;
+    apigateway.resMap[key] = res;
+  }
+  next();
 }
 
 function writePacket(req, res, next) {
-    try {
-        if (req.path !== "/favicon.ico") {
-            const appName = req.path.split("/")[2];
+  try {
+    if (req.path !== "/favicon.ico") {
+      const appName = req.path.split("/")[2];
 
-            apigateway.appClientMap[appName].write(req.packet);
-        }
-    } catch (e) {
-        let error = new Error('ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRR');
-
-        apigateway.resMap[req.resKey].status(error.status || 500);
-        apigateway.resMap[req.resKey].send(error.message || 'ErrorRRRRRRRRRRRRRRRRRRRRRRRRRRRR!!');
-        delete apigateway.resMap[req.resKey];
+      apigateway.appClientMap[appName].write(req.packet);
     }
+  } catch (e) {
+    let error = new Error("ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRR");
+
+    apigateway.resMap[req.resKey].status(error.status || 500);
+    apigateway.resMap[req.resKey].send(
+      error.message || "ErrorRRRRRRRRRRRRRRRRRRRRRRRRRRRR!!"
+    );
+    delete apigateway.resMap[req.resKey];
+  }
 }
 
+const searchRouter = require("./routes/search")(apigateway);
 
+server.use(express.json());
+server.use(cors());
 
-let searchRouter = require("./routes/search")(apigateway)
+server.use(favicon(path.join(__dirname, "/favicon.ico")));
+server.use(setResponseKey);
 
-server.use(setResponseKey)
+server.get("/", (req, res) => res.send("Hello World!"));
 
-server.get('/', (req, res) => res.send('Hello World!'));
-
-server.use('/api/search', searchRouter);
-
-
+server.use("/api/search", searchRouter);
 
 server.use(writePacket);
 
 server.listen(GATE_EXPRESS_PORT, async () => {
-    connectToAllApps();
-})
+  connectToAllApps();
+});
 
 async function makeAppClient(name) {
-    try {
-        const client = await apigateway.connectToApp(name,
-            () => {
-                // connect이벤트 함수
-                apigateway.appClientMap[name] = client;
-                apigateway.icConnectMap[name] = true;
-                console.log(`${name} service connect`)
-            },
-            (data) => {
-                // data이벤트 함수
-                if (data.method === "REPLY") {
-                    apigateway.resMap[data.key].json(data.body.studygroups);
-                }
-                if (data.method === "ERROR") {
-                    let error = new Error('ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRR');
+  try {
+    const client = await apigateway.connectToApp(
+      name,
+      () => {
+        // connect이벤트 함수
+        apigateway.appClientMap[name] = client;
+        apigateway.icConnectMap[name] = true;
+        console.log(`${name} service connect`);
+      },
+      data => {
+        // data이벤트 함수
+        if (data.method === "REPLY") {
+          apigateway.resMap[data.key].json(data.body.studygroups);
+        }
+        if (data.method === "ERROR") {
+          let error = new Error("ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRR");
 
-                    apigateway.resMap[data.key].status(error.status || 500);
-                    apigateway.resMap[data.key].send(error.message || 'ErrorRRRRRRRRRRRRRRRRRRRRRRRRRRRR!!');
-                }
-                delete apigateway.resMap[data.key];
-            },
-            () => {
-                apigateway.icConnectMap[name] = false;
-                console.log(`${name} service end`)
-            },
-            () => {
-                apigateway.icConnectMap[name] = false;
-                console.log(`${name} service error`)
-            }
-        );
+          apigateway.resMap[data.key].status(error.status || 500);
+          apigateway.resMap[data.key].send(
+            error.message || "ErrorRRRRRRRRRRRRRRRRRRRRRRRRRRRR!!"
+          );
+        }
+        delete apigateway.resMap[data.key];
+      },
+      () => {
+        apigateway.icConnectMap[name] = false;
+        console.log(`${name} service end`);
+      },
+      () => {
+        apigateway.icConnectMap[name] = false;
+        console.log(`${name} service error`);
+      }
+    );
 
-        setInterval(() => {
-            if (!apigateway.icConnectMap[name]) {
-                console.log(`try connect to search2`);
-                client.connect();
-            }
-        }, 2000);
-        return client;
-    } catch (e) {
-        console.log(e)
-    }
+    setInterval(() => {
+      if (!apigateway.icConnectMap[name]) {
+        console.log(`try connect to ${name}`);
+        client.connect();
+      }
+    }, 2000);
+    return client;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 /**
@@ -186,4 +193,3 @@ async function connectToAllApps() {
     makeAppClient(appName);
   });
 }
-
