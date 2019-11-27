88f8db00dbce2e89510b8d7dfca8d50174b7f3ea
plm9606 <plm9606@naver.com>
2019-11-27 21:12:45 +0900

    feat(server/log): 로그 서비스 생성
    데이터는 수신만하고 응답 패킷은 보내지 않는다
    추후 로그를 취합하고 정재하여 elasticsearch에 저장하는 로직을 구현한다

---
+++ server/services/log/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config({ path: "./server/services/log/.env.log" });
+const { LOG_PORT } = process.env;
+const { makePacket } = require("../../lib/tcp/util");
+
+/**
+ * TODO: elasticsearch로 데이터 정제하여 보내기
+ */
+class LogService extends require("../../lib/tcp/App") {
+  constructor(name, host, port) {
+    super(name, host, port);
+    this.query = [];
+  }
+
+  async onRead(socket, data) {
+    const { key } = data;
+
+    console.log(`LOG:: ${JSON.stringify(data.body)}`);
+
+    const replyPacket = makePacket(
+      "REPLY",
+      "log",
+      {},
+      { data: "it's finall data" },
+      key,
+      this.context
+    );
+
+    // socket.write(replyPacket);
+  }
+}
+
+const logService = new LogService("log", "127.0.0.1", LOG_PORT);
+
+logService.connectToAppListManager();
