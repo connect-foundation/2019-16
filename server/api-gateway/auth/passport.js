8327ae35131195fe9c7b3e9b67fdf16e27ba4c75
김세진 <sejjj120@gmail.com>
2019-11-28 02:29:00 +0900

    feat(gateway/passport): 사용자가 기존에 카카오 로그인을 한 적이 없다면 디비에 새로 추가

--- server/api-gateway/auth/passport.js
+++ server/api-gateway/auth/passport.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const KakaoStrategy = require("passport-kakao").Strategy;
+const User = require("../models/user");
 
 function config(server) {
   passport.use(
@@ -12,18 +13,36 @@ function config(server) {
       },
       function(accessToken, refreshToken, params, profile, done) {
         // authorization 에 성공했을때의 액션
-        const obj = {};
+        const user = {};
 
+        user.email = profile._json.kakao_account.email;
+        user.gender = profile._json.kakao_account.gender;
+        user.ageRange = profile._json.kakao_account.age_range;
+        user.name = profile.username;
+        user.profileImage = profile._json.properties.profile_image;
+        user.birthday = profile._json.kakao_account.birthday;
         user.role = "user";
 
-        return done(null, obj);
+        return done(null, user);
       }
     )
   );
 
   passport.serializeUser(function(user, done) {
+    User.findOne({ email: user.email }, (err, res) => {
+      if (!res)
+        User.create({
+          email: user.email,
+          gender: user.gender,
+          ageRange: user.ageRange,
+          history: [],
+          ownGroups: [],
+          partipatedGroups: []
+        });
+    });
     done(null, user);
   });
+
   passport.deserializeUser(function(obj, done) {
     done(null, obj);
   });
