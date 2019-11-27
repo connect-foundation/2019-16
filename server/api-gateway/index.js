256b469e155df32df1023a0ac0e97207edd825ea
김세진 <sejjj120@gmail.com>
2019-11-27 16:24:06 +0900

    style(gateway): 프리티어 적용

--- server/api-gateway/index.js
+++ server/api-gateway/index.js
@@ -88,91 +88,90 @@ async function makeAppClient(name) {
   } catch (e) {
     console.log(e);
   }
-    if (req.path !== "/favicon.ico") {
-        const key = await makeKey(req.client);
+  if (req.path !== "/favicon.ico") {
+    const key = await makeKey(req.client);
 
-        req.resKey = key;
-        apigateway.resMap[key] = res;
-    }
-    next();
+    req.resKey = key;
+    apigateway.resMap[key] = res;
+  }
+  next();
 }
 
 function writePacket(req, res, next) {
-    try {
-        if (req.path !== "/favicon.ico") {
-            const appName = req.path.split("/")[2];
+  try {
+    if (req.path !== "/favicon.ico") {
+      const appName = req.path.split("/")[2];
 
-            apigateway.appClientMap[appName].write(req.packet);
-        }
-    } catch (e) {
-        let error = new Error('ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRR');
-
-        apigateway.resMap[req.resKey].status(error.status || 500);
-        apigateway.resMap[req.resKey].send(error.message || 'ErrorRRRRRRRRRRRRRRRRRRRRRRRRRRRR!!');
-        delete apigateway.resMap[req.resKey];
+      apigateway.appClientMap[appName].write(req.packet);
     }
+  } catch (e) {
+    let error = new Error("ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRR");
+
+    apigateway.resMap[req.resKey].status(error.status || 500);
+    apigateway.resMap[req.resKey].send(
+      error.message || "ErrorRRRRRRRRRRRRRRRRRRRRRRRRRRRR!!"
+    );
+    delete apigateway.resMap[req.resKey];
+  }
 }
 
+server.use(setResponseKey);
 
-
-let searchRouter = require("./routes/search")(apigateway)
-
-server.use(setResponseKey)
-
-server.get('/', (req, res) => res.send('Hello World!'));
-
-server.use('/api/search', searchRouter);
-
-
+server.get("/", (req, res) => res.send("Hello World!"));
+server.use("/auth", authRouter);
+server.use("/api/search", searchRouter);
 
 server.use(writePacket);
 
 server.listen(GATE_EXPRESS_PORT, async () => {
-    connectToAllApps();
-})
+  connectToAllApps();
+});
 
 async function makeAppClient(name) {
-    try {
-        const client = await apigateway.connectToApp(name,
-            () => {
-                // connect이벤트 함수
-                apigateway.appClientMap[name] = client;
-                apigateway.icConnectMap[name] = true;
-                console.log(`${name} service connect`)
-            },
-            (data) => {
-                // data이벤트 함수
-                if (data.method === "REPLY") {
-                    apigateway.resMap[data.key].json(data.body.studygroups);
-                }
-                if (data.method === "ERROR") {
-                    let error = new Error('ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRR');
+  try {
+    const client = await apigateway.connectToApp(
+      name,
+      () => {
+        // connect이벤트 함수
+        apigateway.appClientMap[name] = client;
+        apigateway.icConnectMap[name] = true;
+        console.log(`${name} service connect`);
+      },
+      data => {
+        // data이벤트 함수
+        if (data.method === "REPLY") {
+          apigateway.resMap[data.key].json(data.body.studygroups);
+        }
+        if (data.method === "ERROR") {
+          let error = new Error("ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRR");
 
-                    apigateway.resMap[data.key].status(error.status || 500);
-                    apigateway.resMap[data.key].send(error.message || 'ErrorRRRRRRRRRRRRRRRRRRRRRRRRRRRR!!');
-                }
-                delete apigateway.resMap[data.key];
-            },
-            () => {
-                apigateway.icConnectMap[name] = false;
-                console.log(`${name} service end`)
-            },
-            () => {
-                apigateway.icConnectMap[name] = false;
-                console.log(`${name} service error`)
-            }
-        );
+          apigateway.resMap[data.key].status(error.status || 500);
+          apigateway.resMap[data.key].send(
+            error.message || "ErrorRRRRRRRRRRRRRRRRRRRRRRRRRRRR!!"
+          );
+        }
+        delete apigateway.resMap[data.key];
+      },
+      () => {
+        apigateway.icConnectMap[name] = false;
+        console.log(`${name} service end`);
+      },
+      () => {
+        apigateway.icConnectMap[name] = false;
+        console.log(`${name} service error`);
+      }
+    );
 
-        setInterval(() => {
-            if (!apigateway.icConnectMap[name]) {
-                console.log(`try connect to search2`);
-                client.connect();
-            }
-        }, 2000);
-        return client;
-    } catch (e) {
-        console.log(e)
-    }
+    setInterval(() => {
+      if (!apigateway.icConnectMap[name]) {
+        console.log(`try connect to search2`);
+        client.connect();
+      }
+    }, 2000);
+    return client;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 /**
@@ -187,4 +186,3 @@ async function connectToAllApps() {
     makeAppClient(appName);
   });
 }
-
