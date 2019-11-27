17011d9f193c8ba82ab8e7f40b25edf474a79354
soob-forest <dltntnqo@naver.com>
2019-11-27 23:10:59 +0900

    fix(search): map으로 만들어놓고 쓰질 않고 있었음
    switch문에서 const map으로 변경

--- server/services/search/Search.js
+++ server/services/search/Search.js
@@ -36,33 +36,11 @@ class Search extends App {
     const { params, query, key } = data;
 
     try {
-      let result;
+      const result = await queryMap[query](params);
 
-      switch (query) {
-        case "searchAllStudyGroup":
-          result = await queryMap.searchAllStudyGroup(params);
-          break;
-        case "searchStudyGroup":
-          result = await queryMap.searchStudyGroup(params);
-          break;
-        case "tagStudyGroup":
-          result = await queryMap.tagStudyGroup(params);
-          break;
-        case "tagStudyGroupWithCategory":
-          result = await queryMap.tagStudyGroupWithCategory(params);
-          break;
-        case "searchStudyGroupWithCategory":
-          result = await queryMap.searchStudyGroupWithCategory(params);
-          break;
-        case "searchAllStudyGroupWithCategory":
-          result = await queryMap.searchAllStudyGroupWithCategory(params);
-          break;
-        default:
-          throw Error("잘못된 Query 입니다.")
-      }
-      packet = makePacket("REPLY", "searchedStudyGroups", {}, { studygroups: result }, key, this.context);
+      packet = makePacket("REPLY", query, {}, { studygroups: result }, key, this.context);
     } catch (e) {
-      packet = makePacket("ERROR", "searchedStudyGroups", {}, { message: e }, key, this.context);
+      packet = makePacket("ERROR", query, {}, { message: e }, key, this.context);
     } finally {
       this.send(socket, packet);
     }
