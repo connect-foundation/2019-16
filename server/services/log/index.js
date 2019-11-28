a4defa86ae7b7866004ae990772f28b64860c503
plm9606 <plm9606@naver.com>
2019-11-29 03:22:26 +0900

    chore(server/log): 로그서비스에 elasticsearch 모듈 설치및 서버 연동

--- server/services/log/index.js
+++ server/services/log/index.js
@@ -1,6 +1,10 @@
 require("dotenv").config({ path: "./server/services/log/.env.log" });
 const { LOG_PORT } = process.env;
-const { makePacket } = require("../../lib/tcp/util");
+const elasticsearch = require("elasticsearch");
+const elasticClient = new elasticsearch.Client({
+  host: "http://210.89.189.171:9200/",
+  log: "trace"
+});
 
 /**
  * TODO: elasticsearch로 데이터 정제하여 보내기
@@ -9,21 +13,33 @@ class LogService extends require("../../lib/tcp/App") {
   constructor(name, host, port) {
     super(name, host, port);
     this.query = [];
+    this.logMap = {};
   }
 
   async onRead(socket, data) {
-    const { key } = data;
+    const { method, query, body } = data;
+    const { timestamp } = body.data;
 
-    console.log(`LOG:: ${JSON.stringify(data.body)}`);
-
-    const replyPacket = makePacket(
-      "REPLY",
-      "log",
-      {},
-      { data: "it's finall data" },
-      key,
-      this.context
+    console.log(data);
+    const jsonData = await new Promise(resolve =>
+      resolve(JSON.stringify(body.data))
     );
+
+    if (query === "log" && method === "POST") {
+      elasticClient.index({
+        index: "test",
+        body: jsonData,
+        id: timestamp
+      });
+    }
+    // const replyPacket = makePacket(
+    //   "REPLY",
+    //   "log",
+    //   {},
+    //   { data: "it's finall data" },
+    //   key,
+    //   this.context
+    // );
 
     // socket.write(replyPacket);
   }
