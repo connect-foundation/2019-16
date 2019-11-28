856a3cd57972aaf24f1d8edc7824d1b6d3982001
Taehyun Lim <dlatns0201@naver.com>
2019-11-28 17:50:52 +0900

    feat(service/study_group): 스터디 개설 요청 데이터 확인

--- server/services/studygroup/StudyGroup.js
+++ server/services/studygroup/StudyGroup.js
@@ -1,5 +1,6 @@
 const App = require("../../lib/tcp/App");
 const { makePacket } = require("../../lib/tcp/util");
+const StudyGroups = require("./models/StudyGroups");
 
 class StudyGroup extends App {
   constructor(name, host, port) {
@@ -7,11 +8,15 @@ class StudyGroup extends App {
   }
   async onRead(socket, data) {
     const { params, query, key } = data;
-    let result;
 
     switch (query) {
       case "addGroup":
-        //
+        params.payload.thumbnail = "testSrc";
+        try {
+          await StudyGroups.create(params.payload);
+        } catch (e) {
+          console.error(e);
+        }
         break;
 
       default:
--- server/services/studygroup/models/Users.js
+++ server/services/studygroup/models/Users.js
@@ -3,7 +3,7 @@ const { Types, Schema, model } = require("mongoose");
 const UserSchema = new Schema({
   email: { type: String, required: true },
   gender: { type: String },
-  ageRange: { type: String },
+  ageRange: { type: Number },
   history: { type: [Types.ObjectId] },
   ownGroups: { type: [Types.ObjectId] },
   partipatedGroups: { type: [Types.ObjectId] }
