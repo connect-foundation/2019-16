5452c4c96091dc613f6b0cebf066cb6dbe560644
Taehyun Lim <dlatns0201@naver.com>
2019-12-03 15:25:51 +0900

    feat(study_search_navbar): 모두 보기 버튼 활성화

--- client/src/components/users/studySearchNavbar/index.jsx
+++ client/src/components/users/studySearchNavbar/index.jsx
@@ -1,8 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useCallback } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import axios from "axios";
+
+import { REQUEST_URL } from "../../../config.json";
 import StudyNavbarItem from "./StudyNavbarItem";
 import { UserContext } from "../../../pages/users/index";
+import { set_groups } from "../../../reducer/users/index";
 
 const Navbar = styled.div`
   padding: 5%;
@@ -18,11 +21,30 @@ const Navbar = styled.div`
     font-weight: bold;
     font-size: 1.7em;
   }
+
+  .navbar-item {
+    cursor: pointer;
+  }
 `;
 
 const StudySearchNavbar = () => {
-  const { userIndexState } = useContext(UserContext);
+  const { userIndexState, userIndexDispatch } = useContext(UserContext);
   const { primaryCategories, secondaryCategories } = userIndexState;
+
+  const searchAllGroups = useCallback(() => {
+    axios.get(`${REQUEST_URL}/search/all/true`).then(result => {
+      const { data } = result;
+
+      for (let i = 0; i < data.length; i++) {
+        data[i].id = i;
+        data[
+          i
+        ].location = `위도: ${data[i].location.lat}, 경도: ${data[i].location.lon}`;
+      }
+
+      userIndexDispatch(set_groups(data));
+    });
+  }, []);
 
   return (
     <Navbar>
@@ -33,9 +55,9 @@ const StudySearchNavbar = () => {
       >
         <div id="navbarExampleTransparentExample" style={{ width: "100%" }}>
           <div className="navbar-start">
-            <Link className="navbar-item" to="/">
+            <span className="navbar-item is-size-3" onClick={searchAllGroups}>
               모두 보기
-            </Link>
+            </span>
 
             {primaryCategories.map(category => (
               <StudyNavbarItem
