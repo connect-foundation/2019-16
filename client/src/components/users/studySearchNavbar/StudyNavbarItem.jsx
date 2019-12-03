3521fd7e935a31e78cfd93a6cdd21ff64e7e20c6
Taehyun Lim <dlatns0201@naver.com>
2019-12-03 15:44:25 +0900

    feat(study_search_navbar): 카테고리 버튼 활성화

--- client/src/components/users/studySearchNavbar/StudyNavbarItem.jsx
+++ client/src/components/users/studySearchNavbar/StudyNavbarItem.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useCallback, useContext } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { REQUEST_URL } from "../../../config.json";
+import { UserContext } from "../../../pages/users/index";
+import { set_groups } from "../../../reducer/users/index";
 
 const Category = styled.div`
   a {
@@ -8,19 +11,45 @@ const Category = styled.div`
     font-weight: bold;
     font-size: 1.7em;
   }
+
+  .navbar-item {
+    cursor: pointer;
+  }
 `;
 
 const StudyNavbarItem = ({ primaryCategory, secondaryCategories }) => {
+  const { userIndexDispatch } = useContext(UserContext);
+
+  const searchGroups = useCallback(e => {
+    const categoryName = e.target.textContent.trim();
+
+    axios.get(`${REQUEST_URL}/search/all/${categoryName}/true`).then(result => {
+      const { data } = result;
+
+      for (let i = 0; i < data.length; i++) {
+        data[i].id = i;
+        data[
+          i
+        ].location = `위도: ${data[i].location.lat}, 경도: ${data[i].location.lon}`;
+      }
+
+      userIndexDispatch(set_groups(data));
+    });
+  }, []);
+
   const itemList = secondaryCategories.map(category => (
-    <Link className="navbar-item" to={`/category/${category}`}>
+    <span className="navbar-item is-size-3" onClick={searchGroups}>
       {category}
-    </Link>
+    </span>
   ));
 
   return (
     <Category>
       <div className="navbar-item has-dropdown is-hoverable">
-        <span className="navbar-link is-arrowless is-size-3">
+        <span
+          className="navbar-link is-arrowless is-size-3"
+          onClick={searchGroups}
+        >
           {primaryCategory}
         </span>
         <div className="navbar-dropdown is-boxed">{itemList}</div>
