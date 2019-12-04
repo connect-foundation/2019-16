e6add9033109a0f364c047017e6cf9d94636ae9d
soob-forest <dltntnqo@naver.com>
2019-12-05 02:26:53 +0900

    fix(app): query 대신에 job 함수를 생성자로 받는다
    doMessageJob함수 내부에서 job함수를 실행 해 messagequeue 를 비울 수 있도록 한다.

---
+++ server/lib/tcp/App.js
@@ -0,0 +1,197 @@
+const TcpServer = require("./tcpServer");
+const TcpClient = require("./tcpClient");
+const { makePacket } = require("../tcp/util");
+const { getAppbyName, getAllApps, popMessageQueue } = require("../redis");
+const { makeLogSender } = require("./logUtils");
+
+class App extends TcpServer {
+  constructor(name, host, port, job) {
+    super(name, host, port);
+    this.job = job;
+    this.isConnectToAppListManager = false;
+    this.isConnectedToLogService = false;
+    this.isConnectToApiGateway = false;
+    this.appClients = {};
+    this.ApiGateway = this.connectToApiGateway();
+
+    this.tcpLogSender = makeLogSender.call(this, "tcp");
+
+  }
+
+  async doMessageJob(job) {
+    const packets = await popMessageQueue(this.name, 1000);
+
+    if (!Array.isArray(packets)) job({}, JSON.parse(packets));
+    packets.forEach((packet) => {
+      job({}, JSON.parse(packet));
+    })
+  }
+
+  send(appClient, data) {
+    const packet = makePacket(
+      data.method,
+      data.curQuery,
+      data.endQuery,
+      data.params,
+      data.body,
+      data.key,
+      data.info
+    );
+
+    /**
+     * params
+     * @param {string} query : 서비스의 쿼리
+     * @param {object} parentData : 해당 서비스를 호출한 서비스 정보
+     */
+    this.tcpLogSender = makeLogSender.call(this, "tcp");
+
+    if (data.curQuery === data.endQuery) {
+      this.ApiGateway.write(packet);
+    } else {
+      appClient.write(packet);
+    }
+  }
+
+  async connectToApp(name, onCreate, onRead, onEnd, onError) {
+    if (this.appClients[name] !== undefined) return this.appClients[name];
+
+    try {
+      const clientInfo = await getAppbyName(name);
+
+      if (clientInfo === null) throw new Error(`${name} server is not running`);
+
+      const client = new TcpClient(
+        clientInfo.host,
+        clientInfo.port,
+        onCreate,
+        onRead,
+        onEnd,
+        onError
+      );
+
+      this.appClients[name] = client;
+      return client;
+    } catch (e) {
+      return e;
+    }
+  }
+
+  async getAllApps() {
+    try {
+      const apps = await getAllApps();
+
+      return apps;
+    } catch (e) {
+      return e;
+    }
+  }
+
+  connectToAppListManager() {
+    this.appListManager = new TcpClient(
+      "127.0.0.1",
+      8100,
+      () => {
+        this.isConnectToAppListManager = true;
+        const packet = makePacket(
+          "POST",
+          "add",
+          "add",
+          {},
+          {},
+          "",
+          this.context
+        );
+
+        this.appListManager.write(packet);
+        console.log(
+          `${this.context.host}:${this.context.port} is connected to app list manager`
+        );
+      },
+      () => {
+        console.log(`It is read function at Port:${this.context.port}`);
+      },
+      () => {
+        console.log(`end service`);
+        this.isConnectToAppListManager = false;
+      },
+      () => {
+        console.log(`App list manager server is down`);
+        this.isConnectToAppListManager = false;
+      }
+    );
+
+    setInterval(() => {
+      if (!this.isConnectToAppListManager) {
+        console.log(`try connect to app list manager`);
+        this.appListManager.connect();
+      }
+    }, 1000);
+    return this.appListManager;
+  }
+
+  connectToLogService() {
+    this.logService = new TcpClient(
+      "127.0.0.1",
+      8004,
+      () => {
+        console.log(
+          `${this.context.host}:${this.context.port} is connected to logService`
+        );
+        this.isConnectedToLogService = true;
+      },
+      () => {
+        console.log(`It is read function at Port:${this.context.port}`);
+      },
+      () => {
+        console.log(`end logService`);
+        this.isConnectedToLogService = false;
+      },
+      () => {
+        console.log(`logService is down`);
+        this.isConnectedToLogService = false;
+      }
+    );
+
+    setInterval(() => {
+      if (!this.isConnectedToLogService) {
+        console.log(`try connect to LogService`);
+        this.logService.connect();
+      }
+    }, 1000);
+    return this.logService;
+  }
+
+  connectToApiGateway() {
+    this.ApiGateway = new TcpClient(
+      "127.0.0.1",
+      8001,
+      () => {
+        this.isConnectToApiGateway = true;
+        console.log(
+          `${this.context.host}:${this.context.port} is connected to ApiGateway`
+        );
+      },
+      () => {
+        console.log(`It is read function at Port:${this.context.port}`);
+      },
+      () => {
+        console.log(`end ApiGateway`);
+        this.isConnectToApiGateway = false;
+      },
+      () => {
+        console.log(`ApiGateway server is down`);
+        this.isConnectToApiGateway = false;
+      }
+    );
+
+    setInterval(() => {
+      if (!this.isConnectToApiGateway) {
+        console.log(`try connect to ApiGateway`);
+        this.ApiGateway.connect();
+      }
+    }, 1000);
+    return this.ApiGateway;
+  }
+}
+
+module.exports = App;
