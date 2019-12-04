8a0cc7bd55a2d947a7d8a01c5f12ccb8ff1351a0
Taehyun Lim <dlatns0201@naver.com>
2019-12-05 02:12:43 +0900

    feat(study_group): app 미들웨어 변경으로 인한 스터디 개설 서비스  수정

--- server/services/studygroup/StudyGroup.js
+++ server/services/studygroup/StudyGroup.js
@@ -1,5 +1,4 @@
 const App = require("../../lib/tcp/App");
-const { makePacket } = require("../../lib/tcp/util");
 const StudyGroups = require("./models/StudyGroups");
 const { pushStudyGroups } = require("../../lib/redis");
 
@@ -8,10 +7,10 @@ class StudyGroup extends App {
     super(name, host, port);
   }
   async onRead(socket, data) {
-    const { params, query, key } = data;
-    let packet;
+    const { params, curQuery } = data;
+    let replyData = data;
 
-    switch (query) {
+    switch (curQuery) {
       case "addGroup":
         try {
           const groupInfo = params.payload;
@@ -19,32 +18,20 @@ class StudyGroup extends App {
           groupInfo.members = [];
           groupInfo.now_personnel = 1;
           groupInfo.days = groupInfo.selectedDays;
-          groupInfo.location = { lat: 41.12, lon: -50.34 };
-
-          await StudyGroups.create(params.payload);
-
-          groupInfo.leader = groupInfo.leader.name;
-
+          groupInfo.endTime = groupInfo.startTime + groupInfo.during;
           delete groupInfo.selectedDays;
+          delete groupInfo.during;
 
-          // const testGroups = [
-          //   { "title": "오늘도 코딩 스터디", "leader": "리더일", "members": [{ "email": "sdkf@naver.com" }, { "email": "sjdklf@kakao.com" }], "min_personnel": 2, "max_personnel": 6, "isRecruiting": true, "intro": "일주일에 3번 만나 모각코 합니다", "thumbnail": "으쌰으쌰", "location": { "lat": 41.12, "lon": -71.34 }, "tags": ["자바", "파이썬" ] },
-          //   { "title": "어제도 코딩 스터디", "leader": "리더삼", "members": [{ "email": "sdzxckf@naver.com" }, { "email": "sjdkqweqwelf@kakao.com" }], "min_personnel": 4, "max_personnel": 9, "isRecruiting": true, "intro": "매일매일 만나고 싶지만 일주일에 한 번", "thumbnail": "고고고", "location": { "lat": 41.12, "lon": -50.34 }, "tags": [ "GO" ] }
-          // ]
-
+          await StudyGroups.create(groupInfo);
           await pushStudyGroups(groupInfo);
 
-          packet = makePacket(
-            "REPLY",
-            query,
-            {},
-            { href: "/" },
-            key,
-            this.context
-          );
+          replyData.method = "REPLY";
+          replyData.body = { href: "/" };
         } catch (e) {
           console.error(e);
-          packet = makePacket("ERROR", query, {}, {}, key, this.context);
+          replyData.method = "ERROR";
+          replyData.body = e;
+          // packet = makePacket("ERROR", query, {}, {}, key, this.context);
         }
         break;
 
@@ -52,10 +39,7 @@ class StudyGroup extends App {
         break;
     }
 
-    this.send(socket, packet);
-  }
-  send(socket, packet) {
-    socket.write(packet);
+    this.send(socket, replyData);
   }
 }
 
