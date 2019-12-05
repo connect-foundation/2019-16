cd8ced321bee3fe5003d28ca4c2141eab0f812b4
Taehyun Lim <dlatns0201@naver.com>
2019-12-05 16:05:32 +0900

    refactor(service/study_group): 컨트롤러를 만들어 라우팅 간결화

---
+++ server/api-gateway/routes/api/studyGroup/ctrl.js
@@ -0,0 +1,57 @@
+const { makePacket } = require("../../../../lib/tcp/util");
+
+exports.uploadToImage = (storage, path, bucketName, bucketLink) => async (
+  req,
+  res,
+  next
+) => {
+  const image = req.file;
+  const imageSrc = `${path}/${Date.now()}${image.originalname}`;
+  const imageLink = bucketLink + imageSrc;
+
+  console.log(bucketName);
+
+  try {
+    await storage
+      .putObject({
+        Bucket: bucketName,
+        Key: `${path}/`
+      })
+      .promise();
+
+    await storage
+      .putObject({
+        Bucket: bucketName,
+        Key: imageSrc,
+        ACL: "public-read",
+        Body: image.buffer
+      })
+      .promise();
+
+    req.imageLink = imageLink;
+    console.log("good\n\n\n\n\n\n");
+    next();
+  } catch (e) {
+    console.error(e);
+    throw new Error(e);
+  }
+};
+
+exports.sendGroupCreationPacket = apigateway => (req, res, next) => {
+  const payload = JSON.parse(req.body.data);
+
+  payload.thumbnail = req.imageLink;
+
+  const packet = makePacket(
+    "POST",
+    "addGroup",
+    "addGroup",
+    { payload },
+    {},
+    req.resKey,
+    apigateway.context
+  );
+
+  req.packet = packet;
+  next();
+};
