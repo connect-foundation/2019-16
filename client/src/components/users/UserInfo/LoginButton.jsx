3cd213555b7c475fad290087c8dc3c4f28003fde
김세진 <sejjj120@gmail.com>
2019-12-05 13:34:29 +0900

    fix(client/loginbutton): 로그인 후 재렌더링이 안되는 문제 해결
    useState를 사용할 때 기존의 객체를 사용해서 랜더링 되지 않은 문제를 해결.

--- client/src/components/users/UserInfo/LoginButton.jsx
+++ client/src/components/users/UserInfo/LoginButton.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useCallback } from "react";
 import styled from "styled-components";
 import KakaoLogin from "react-kakao-login";
 import axios from "axios";
@@ -15,51 +15,51 @@ const KakaoLoginButton = styled(KakaoLogin)`
   }
 `;
 
-const userInfoParser = ({ profile, response }) => {
-  const { kakao_account, properties } = profile;
-
-  return {
-    userAgeRange: +kakao_account.age_range[0],
-    userName: properties.nickname,
-    userEmail: kakao_account.email,
-    userGender: kakao_account.gender,
-    profileImage: properties.profile_image,
-    accessToken: response.access_token
-  };
-};
-
 const LoginButton = () => {
   const { userInfo, setUserInfo } = useContext(UserContext);
+
+  const userInfoParser = useCallback(({ profile, response }) => {
+    const { kakao_account, properties } = profile;
+
+    return {
+      userAgeRange: +kakao_account.age_range[0],
+      userName: properties.nickname,
+      userEmail: kakao_account.email,
+      userGender: kakao_account.gender,
+      profileImage: properties.profile_image,
+      accessToken: response.access_token
+    };
+  }, []);
+
+  const onSuccess = ({ response, profile }) => {
+    const email = profile.kakao_account.email;
+
+    // axios
+    //   .post(`${REQUEST_URL}/auth/users/checkEmail`, { email })
+    //   .then(({ data }) => {
+    //     if (!data.exist) {
+    //       // 처음 방문한 사용자라면 위치를 입력받아서 데이터베이스에 저장
+    //       const location = prompt(
+    //         "처음이신가봐요! 거주하고 계신 동네를 말씀해 주세요"
+    //       );
+    //     }
+    //     const tmp = Object.assign(
+    //       userInfo,
+    //       userInfoParser({ response, profile })
+    //     );
+    //     setUserInfo(tmp);
+    //   })
+    //   .catch(e => {
+    //     console.error(e);
+    //   });
+    setUserInfo({ ...userInfo, ...userInfoParser({ response, profile }) });
+  };
 
   return (
     <KakaoLoginButton
       className="button is-warning is-rounded"
       jsKey={KAKAO_JS_KEY}
-      onSuccess={({ response, profile }) => {
-        const email = profile.kakao_account.email;
-
-        // axios
-        //   .post(`${REQUEST_URL}/auth/users/checkEmail`, { email })
-        //   .then(({ data }) => {
-        //     if (!data.exist) {
-        //       // 처음 방문한 사용자라면 위치를 입력받아서 데이터베이스에 저장
-        //       const location = prompt(
-        //         "처음이신가봐요! 거주하고 계신 동네를 말씀해 주세요"
-        //       );
-        //     }
-        //     const tmp = Object.assign(
-        //       userInfo,
-        //       userInfoParser({ response, profile })
-        //     );
-        //     setUserInfo(tmp);
-        //   })
-        //   .catch(e => {
-        //     console.error(e);
-        //   });
-        setUserInfo(
-          Object.assign(userInfo, userInfoParser({ response, profile }))
-        );
-      }}
+      onSuccess={onSuccess}
       onFailure={res => {
         // Caution popup
         console.error(res);
