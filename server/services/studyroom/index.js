26b2a48a60d1450f4573f026f6d1b66eb736875c
aram <arame@kakao.com>
2019-12-05 23:11:27 +0900

    feat(server/studyroom): 스터디룸 몽고 schema 생셩 및 디비 연결

--- server/services/studyroom/index.js
+++ server/services/studyroom/index.js
@@ -1,7 +1,27 @@
 require("dotenv").config();
 const StudyRoom = require("./StudyRoom");
+const mongoose = require("mongoose");
 
-const { STUDYROOM_NAME, STUDYROOM_HOST, STUDYROOM_PORT } = process.env;
+const {
+  STUDYROOMS_MONGO_URL,
+  STUDYROOM_NAME,
+  STUDYROOM_HOST,
+  STUDYROOM_PORT
+} = process.env;
+
+mongoose
+  .connect(STUDYROOMS_MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("StudyRoom mongoDB is connected");
+  })
+  .catch(err => {
+    console.log("StudyRoom mongoDB connection fail", err);
+  });
+
 const studyRoom = new StudyRoom(STUDYROOM_NAME, STUDYROOM_HOST, STUDYROOM_PORT);
 
 studyRoom.connectToAppListManager();
