5dc1642da092f7c5e523c95776c1670999b51746
Taehyun Lim <dlatns0201@naver.com>
2019-12-05 14:18:39 +0900

    feat(group_detail): 서버에 그룹 상세 정보 요청하는 로직 추가
    ObjectId를 기반으로 스터디 그룹을 조회 받아 데이터 응답 받음

--- client/src/pages/users/groupDetail.jsx
+++ client/src/pages/users/groupDetail.jsx
@@ -1,11 +1,14 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import styled from "styled-components";
+import axios from "axios";
+import { REQUEST_URL } from "../../config.json";
 import Header from "../../components/users/groupDetail/Header";
 import Main from "../../components/users/groupDetail/Main";
 import Intro from "../../components/users/groupDetail/Intro";
 import {
   groupDetail as groupDetailReducer,
-  initialState
+  initialState,
+  set_detail_data
 } from "../../reducer/users/groupDetail";
 
 const StyledGroupDetail = styled.div`
@@ -16,8 +19,18 @@ const StyledGroupDetail = styled.div`
   margin: 3rem auto;
 `;
 
-const GroupDetail = () => {
+const GroupDetail = props => {
   const [groupData, dispatch] = useReducer(groupDetailReducer, initialState);
+  const groupId = props._id;
+
+  useEffect(() => {
+    axios
+      .get(`${REQUEST_URL}/api/studygroup/detail/${groupId}`)
+      .then(result => {
+        const groupData = result.data;
+        dispatch(set_detail_data(groupData));
+      });
+  }, []);
 
   return (
     <StyledGroupDetail>
