638e743e7bfa5ab5e6b65f08b61b7a931c8367f5
Taehyun Lim <dlatns0201@naver.com>
2019-12-09 00:23:08 +0900

    feat(group_create_page): 그룹 생성 validation 작성

--- client/src/pages/users/groupCreate.jsx
+++ client/src/pages/users/groupCreate.jsx
@@ -77,6 +77,12 @@ const GroupCreate = () => {
       data.leader = userEmail;
       data.location = { lat: 41.12, lon: -50.34 };
       data.endTime = data.startTime + data.during;
+      data.endTime = data.endTime > 24 ? data.endTime - 24 : data.endTime;
+
+      let validationObj = {};
+      if (!(validationObj = validation(data)).isProper)
+        return alert(validationObj.reason);
+
       delete data.during;
 
       form.append("image", data.thumbnail);
@@ -97,7 +103,7 @@ const GroupCreate = () => {
           alert("에러 발생");
         });
     },
-    [state]
+    [state, userEmail]
   );
 
   return (
@@ -161,6 +167,20 @@ const GroupCreate = () => {
       </button>
     </StyledGroupCreate>
   );
+};
+
+const validation = data => {
+  if (data.category.length !== 2 || data.category.some(v => v === null))
+    return { isProper: false, reason: "카테고리 두 개를 선택해주세요" };
+  if (!data.title) return { isProper: false, reason: "제목을 입력해주세요" };
+  if (!data.subtitle)
+    return { isProper: false, reason: "부제목을 입력해주세요" };
+  if (!data.days.length)
+    return { isProper: false, reason: "스터디 요일을 선택해주세요" };
+  if (!data.leader) return { isProper: false, reason: "잘못된 접근입니다." };
+  if (!data.location || Object.values(data.location).length !== 2)
+    return { isProper: false, reason: "위치를 선택해주세요" };
+  return { isProper: true };
 };
 
 export default GroupCreate;
