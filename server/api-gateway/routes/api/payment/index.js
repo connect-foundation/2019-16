17297509f921cc31d80dd02d8ba45282414ddf0c
김세진 <sejjj120@gmail.com>
2019-12-09 17:46:53 +0900

    chore(server): 클라이언트 브랜치에서 서버 작업 폴더 삭제

--- server/api-gateway/routes/api/payment/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const Router = express.Router();
-const payQueueInspection = require("../../../middleware/payment/payQueueInspection");
-const readyToPay = require("../../../middleware/payment/readyToPay");
-
-Router.post("/ready", payQueueInspection, readyToPay);
-
-module.exports = Router;
