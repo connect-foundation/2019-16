6c4d423b3731243eb4d57ad7cb8d953f1da62097
aram <arame@kakao.com>
2019-12-10 20:55:46 +0900

    feat(server/studyroom): 스터디룸 서비스에 job 추가

--- server/services/studyroom/StudyRoom.js
+++ server/services/studyroom/StudyRoom.js
@@ -4,49 +4,50 @@ const StudyRooms = require("./models/studyrooms");
 const convertKntoMile = km => {
   return km / 6378.1;
 };
+
+async function jobEexcutor(socket, data) {
+  const { params, curQuery, endQuery, key } = data;
+
+  const { geopoint, personnel, startTime, endTime, days } = params;
+
+  if (curQuery === "availableRooms") {
+    const filteredRooms = await StudyRooms.find({
+      max_personnel: {
+        $gte: personnel
+      },
+      min_personnel: {
+        $lte: personnel
+      },
+      location: {
+        $geoWithin: {
+          $centerSphere: [
+            [geopoint.latitude, geopoint.longitude],
+            convertKntoMile(1)
+          ]
+        }
+      },
+      open_time: {
+        $lte: startTime
+      },
+      close_time: {
+        $gte: endTime
+      }
+    });
+
+    this.send(socket, {
+      method: "GET",
+      curQuery: "filterStudyGroup",
+      endQuery,
+      params: { filteredRooms, studyGroupInfo: { ...params } },
+      body: {},
+      key
+    });
+  }
+}
+
 class StudyRoom extends App {
   constructor(name, host, port) {
-    super(name, host, port);
-  }
-
-  async onRead(socket, data) {
-    const { method, params, curQuery, endQuery, key } = data;
-
-    const { geopoint, personnel, startTime, endTime, days } = params;
-
-    if (method === "GET" && curQuery === "availableRooms") {
-      const filteredRooms = await StudyRooms.find({
-        max_personnel: {
-          $gte: personnel
-        },
-        min_personnel: {
-          $lte: personnel
-        },
-        location: {
-          $geoWithin: {
-            $centerSphere: [
-              [geopoint.latitude, geopoint.longitude],
-              convertKntoMile(1)
-            ]
-          }
-        },
-        open_time: {
-          $lte: startTime
-        },
-        close_time: {
-          $gte: endTime
-        }
-      });
-
-      this.send(socket, {
-        method: "REPLY",
-        curQuery,
-        endQuery,
-        params: {},
-        body: { filteredRooms },
-        key
-      });
-    }
+    super(name, host, port, jobEexcutor);
   }
 }
 
