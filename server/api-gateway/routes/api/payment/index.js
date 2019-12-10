a980626dcc6ae30859c7a4832878c28cedae2428
김세진 <sejjj120@gmail.com>
2019-12-10 13:34:34 +0900

    refactor(gateway/payment): 네이밍 대문자 변경
    Router에서 router로 변경.

--- server/api-gateway/routes/api/payment/index.js
+++ server/api-gateway/routes/api/payment/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const Router = express.Router();
+const router = express.Router();
 const payQueueInspection = require("../../../middleware/payment/payQueueInspection");
 const readyToPay = require("../../../middleware/payment/readyToPay");
 
-Router.post("/ready", payQueueInspection, readyToPay);
+router.post("/ready", payQueueInspection, readyToPay);
 
-module.exports = Router;
+module.exports = router;
