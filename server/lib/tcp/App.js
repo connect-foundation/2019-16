98227b82be45a6b656e8b55e40a59a527e7402a2
aram <arame@kakao.com>
2019-12-10 16:00:34 +0900

    fix(server/app): doMessagejob 함수 변경
    불필요한 파라미터를 받고 있어 해당 부분 제거함
    job에 this를 바인딩할 필요가 없기 때문에 해당 부분 수정함

--- server/lib/tcp/App.js
+++ server/lib/tcp/App.js
@@ -17,15 +17,15 @@ class App extends TcpServer {
     this.tcpLogSender = makeLogSender.call(this, "tcp");
     (async () => {
       await new Promise(res => this.connectToLogService(res));
-      this.doMessageJob(job);
+      this.doMessageJob();
     })();
   }
 
-  async doMessageJob(job) {
+  async doMessageJob() {
     const packets = await popMessageQueue(this.context.name, 1000);
 
     if (!Array.isArray(packets)) {
-      job.bind(this)({}, JSON.parse(packets));
+      this.job({}, JSON.parse(packets));
     } else {
       packets.forEach(packet => {
         this.job({}, JSON.parse(packet));
@@ -47,13 +47,6 @@ class App extends TcpServer {
       data.key,
       data.info
     );
-
-    /**
-     * params
-     * @param {string} query : 서비스의 쿼리
-     * @param {object} parentData : 해당 서비스를 호출한 서비스 정보
-     */
-    this.tcpLogSender = makeLogSender.call(this, "tcp");
 
     if (data.curQuery === data.endQuery) {
       this.ApiGateway.write(packet);
