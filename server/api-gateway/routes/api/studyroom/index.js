bcd50a7f12df8fa1d29173b28fc7838d72d4a799
soob-forest <dltntnqo@naver.com>
2019-12-11 14:20:33 +0900

    fix: styudyroom.js 내용 api폴더로 옮김

---
+++ server/api-gateway/routes/api/studyroom/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const { makePacket } = require("../../../../lib/tcp/util");
+
+module.exports = function(apigateway) {
+  router.get("/availableRooms", (req, res, next) => {
+    const studyGroupData = req.body;
+
+    req.packet = makePacket(
+      "GET",
+      "apigateway",
+      "availableRooms",
+      "filterStudyGroup",
+      { ...studyGroupData },
+      {},
+      req.resKey,
+      apigateway.context
+    );
+
+    next();
+  });
+  return router;
+};
