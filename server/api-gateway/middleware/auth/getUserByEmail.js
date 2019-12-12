982fd4f59d4365a601032529a887b51a912fa74b
Sejin Kim <sejjj120@gmail.com>
2019-12-12 19:19:09 +0900

    feat(middleware/auth): 사용자 정보 조회 미들웨어 구현
    사용자가 로그인 버튼을 눌렀을 때, 사용자 페이지에 처음 접근 했을 때 요청.

--- server/api-gateway/middleware/auth/getUserByEmail.js
+++ server/api-gateway/middleware/auth/getUserByEmail.js
@@ -1,15 +1,19 @@
 const User = require("../../models/user");
 
-module.exports = function(req, res, next) {
+module.exports = function(req, res) {
   const { email } = req.params;
 
   User.findOne({ email }, (err, user) => {
+    if (err) return res.sendStatus(500);
     if (user === null) return res.json(user);
 
     return res.json({
-      userGender: user.gender || null,
-      userAgeRange: user.ageRange || null,
-      userLocation: user.location || { lat: null, lon: null }
+      userEmail: user.email,
+      userName: user.name,
+      profileImage: user.profileImage,
+      userGender: user.gender,
+      userAgeRange: user.ageRange,
+      userLocation: user.location
     });
   });
 };
