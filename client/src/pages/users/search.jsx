de4252b078edaf554fdfaab3a464031ea7a2545a
soob-forest <dltntnqo@naver.com>
2019-12-12 22:46:07 +0900

    feat: search 페이지 추가

---
+++ client/src/pages/users/search.jsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useContext } from "react";
+import queryString from "query-string";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import StudyGroupCard from "../../components/users/groupCard";
+
+import { set_groups } from "../../reducer/users";
+import { UserContext } from "./index";
+
+const StyledSearch = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 5%;
+  padding-right: 5%;
+
+  .main-page-title{
+    font-family: 'Black Han Sans', sans-serif;
+    color: #000000;
+    padding: 5%;
+    align-self:start;
+    .main-title {
+        font-size: 6em;
+      }
+    .main-subtitle{
+        font-size: 4em;
+        .highlight{
+            color:#e41d60
+        }
+      }
+    }
+  }
+
+  .study-group-list{
+      align-self:center;
+
+      display: flex;
+      flex-direction: row;
+      justify-content: space-evenly;
+
+      background-color: #f8f0ee;
+      width: 75rem;
+      flex-wrap: wrap;
+      margin:0 10%;
+      .study-group-card{
+          margin: 2em;
+      }
+  }
+`;
+
+const isSetPositionDuringLoading = (loading, lat, lon) =>
+  loading && lat !== null && lon !== null;
+
+const isHaveCardDataWhenLoaded = (loading, data) =>
+  !loading && data && data.length;
+
+const Search = ({ location, match }) => {
+  const query = queryString.parse(location.search).query;
+  const {
+    userIndexState,
+    userIndexDispatch,
+    userInfo,
+    getApiAxiosState
+  } = useContext(UserContext);
+  const { myGroups, searchList } = userIndexState;
+  const { userEmail, userLocation } = userInfo;
+
+  let { lat, lon } = userLocation;
+  let { loading, data, error, request } = getApiAxiosState;
+
+  useEffect(() => {
+    isSetPositionDuringLoading(loading, lat, lon) &&
+      request("get", `/search/query/${query}/location/${lat}/${lon}/true`);
+  }, [userLocation]);
+
+  useEffect(() => {
+    isHaveCardDataWhenLoaded(loading, data) &&
+      userIndexDispatch(set_groups(data));
+  }, [data, userLocation]);
+
+  return (
+    <StyledSearch>
+      <div className="study-group-list">
+        {(() => {
+          if (loading) return <h3> 로딩 중... </h3>;
+          if (error) return <h3> 에러 발생 </h3>;
+          if (!data.length) return <h3> 데이터가 업소용 </h3>;
+
+          return searchList.map(groupData => {
+            return (
+              <StudyGroupCard
+                key={groupData.id}
+                groupData={groupData}
+              ></StudyGroupCard>
+            );
+          });
+        })()}
+      </div>
+    </StyledSearch>
+  );
+};
+
+export default Search;
