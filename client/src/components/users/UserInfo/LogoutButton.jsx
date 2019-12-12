72df354c99bd1ee3fb6e141af7d34a8255fce645
Sejin Kim <sejjj120@gmail.com>
2019-12-12 16:26:38 +0900

    feat(client/logoutbtn): 경고창 추가

--- client/src/components/users/UserInfo/LogoutButton.jsx
+++ client/src/components/users/UserInfo/LogoutButton.jsx
@@ -7,6 +7,7 @@ const LogoutButton = () => {
       className="button is-rounded is-warning"
       onClick={() => {
         Cookies.remove("access_token");
+        alert("로그아웃 되었습니다.");
         window.location.href = "/";
       }}
     >
