c9ec8d6acaf51ff64d7c63d327f794d28ee27bce
Sejin Kim <sejjj120@gmail.com>
2019-12-12 16:26:20 +0900

    feat(client/loginbtn): 로그인 성공 후의 콜백 함수
    처음 로그인 했을 때와 아닌 경우를 나눈다.
    처음 로그인을 했다면 사는 지역을 입력 받은 후에 데이터베이스에 저장한다.

--- client/src/components/users/UserInfo/LoginButton.jsx
+++ client/src/components/users/UserInfo/LoginButton.jsx
@@ -1,8 +1,6 @@
 import React, { useContext, useCallback } from "react";
 import styled from "styled-components";
 import KakaoLogin from "react-kakao-login";
-import axios from "axios";
-import Cookies from "js-cookie";
 import { UserContext } from "../../../pages/users";
 import { REQUEST_URL, KAKAO_JS_KEY } from "../../../config.json";
 
@@ -26,6 +24,7 @@ const LoginButton = () => {
     debugger;
 
     return {
+      kakaoAccessToken: response.access_token,
       userAgeRange: +kakao_account.age_range[0] || null,
       userName: properties.nickname,
       userEmail: kakao_account.email || "blank",
@@ -37,20 +36,50 @@ const LoginButton = () => {
 
   const onSuccess = async ({ response, profile }) => {
     const email = profile.kakao_account.email || "blank";
-    const url = `${REQUEST_URL}/auth/users/accounts?email=${email}`;
+    const url = `${REQUEST_URL}/auth/users/accounts/${email}`;
     const options = { method: "GET" };
 
     fetch(url, options)
       .then(r => r.json())
-      .then(res => {
-        if (res) {
-          setUserInfo({
-            ...userInfo,
-            ...userInfoParser({ response, profile })
+      .then(result => {
+        if (result === null) {
+          const locationInput = prompt(
+            "처음 오셨군요! 사는 지역을 말씀해 주세요"
+          );
+          const url = `${REQUEST_URL}/auth/users/register`;
+          const data = {
+            kakaoAccessToken: response.access_token,
+            userEmail: profile.kakao_account.email || "",
+            userName: profile.properties.nickname,
+            userGender: profile.kakao_account.gender || "",
+            userAgeRange: +profile.kakao_account.age_range[0] || null,
+            profileImage:
+              response.properties.profile_image || DEFAULT_PROFILE_IMAGE,
+            userLocation: locationInput
+          };
+          const options = {
+            method: "POST",
+            headers: { "Content-Type": "application/json;charset=utf-8" },
+            body: data
+          };
+
+          fetch(url, options).then(() => {
+            alert("반갑습니다! 로그인을 다시 시도해 주세요");
           });
-        } else {
-          const inputLocation = prompt("사는 지역을 말씀해 주세요");
         }
+
+        const url = `${REQUEST_URL}/auth/users/kakao-access-token`;
+        const options = {
+          method: "PUT",
+          headers: { "Content-Type": "application/json;charset:utf-8" },
+          body: { kakaoAccessToken: response.access_token }
+        };
+
+        fetch(url, options).then(() => {
+          setUserInfo({
+            ...result
+          });
+        });
       })
       .catch(e => {
         alert("데이터 요청을 할 수 없습니다.");
