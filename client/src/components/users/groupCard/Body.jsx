26e69d728c37c87a33d77047c617fa42a487e957
Taehyun Lim <dlatns0201@naver.com>
2019-12-12 16:36:43 +0900

    style(group_card): 태그들을 footer에서 body로 위치시킴
    location부터의 정보들은 모든 카드들이 동일한 선상에 위치하도록 하기 위해
    제목, 부제목, 버튼들을 하나의 div로, 가운데 정렬한 뷰가 더 깔끔해보이기 때문

--- client/src/components/users/groupCard/Body.jsx
+++ client/src/components/users/groupCard/Body.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import styled from "styled-components";
 import Title from "./Title";
 import Subtitle from "./Subtitle";
+import TagButtons from "../common/TagButtons";
 
 const StyledBody = styled.div`
   flex: 1;
+  min-height: 12rem;
   padding: 0 1rem;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+
+  & > *:not(:last-child) {
+    margin-bottom: 1.2rem;
+  }
 `;
 
-const Body = ({ bodyData: { title, subtitle } }) => (
+const Body = ({ bodyData: { title, subtitle, tags } }) => (
   <StyledBody>
     <Title title={title} />
     <Subtitle subtitle={subtitle} />
+    <TagButtons tags={tags} />
   </StyledBody>
 );
 
