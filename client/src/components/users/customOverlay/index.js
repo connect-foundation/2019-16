b37f1939096435b0d5377e3efc9191c96b29b9d5
aram <arame@kakao.com>
2019-12-13 10:01:27 +0900

    refactor(client/studyroom): 지도 커스텀 오버레이에 버튼 추가
    예약하기 버튼 추가
    카페 네임 데이터에 html 텍스트가 들어가 있어 (<h1>스터디</h1>) innerHTML로 넣어줌

--- client/src/components/users/customOverlay/index.js
+++ client/src/components/users/customOverlay/index.js
@@ -6,18 +6,24 @@ const CustomOverlay = ({ data }) => {
   return (
     <div className="overlay">
       <div className="body">
-        <div className="is-size-5 has-text-weight-bold cafe-name">
-          {cafe_name}
-        </div>
+        <div
+          className="is-size-5 has-text-weight-bold cafe-name"
+          dangerouslySetInnerHTML={{ __html: `${cafe_name}` }}
+        ></div>
         <div>
           <span className="has-text-info has-text-weight-bold">Room </span>
           {name}
         </div>
-        <div className="is-size-7">
-          <ul>
-            <li>{`인원 ${min_personnel} ~ ${max_personnel}명`}</li>
-            <li>{`요금 ${price}/1인`}</li>
-          </ul>
+        <div className="info-and-btn-wrapper">
+          <div className="is-size-7">
+            <ul>
+              <li>{`인원 ${min_personnel} ~ ${max_personnel}명`}</li>
+              <li>{`요금 ${price}/1인`}</li>
+            </ul>
+          </div>
+          <button className="reservation-btn button is-primary is-rounded is-focused">
+            예약하기
+          </button>
         </div>
       </div>
       <div className="bottom"></div>
