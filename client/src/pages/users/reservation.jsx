2f2448fd3b50a633d5be1014bc38f0216d6a2bcb
aram <arame@kakao.com>
2019-12-13 10:03:57 +0900

    feat(client/studyroom): 클릭한 마터를 다시 클릭한 경우 오버레이가 없어지도록 이벤트 등록

--- client/src/pages/users/reservation.jsx
+++ client/src/pages/users/reservation.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, Fragment } from "react";
 import styled from "styled-components";
 import useWindowSize from "../../lib/useWindowSize";
 import { makeOverlay, markerImage, hoverImage } from "../../lib/kakaoMapUtils";
+import axios from "axios";
 
 const { kakao } = window;
 var bounds = new kakao.maps.LatLngBounds();
@@ -25,7 +26,13 @@ const Reservation = () => {
   const addMarkerEvent = marker => {
     kakao.maps.event.addListener(marker, "click", function() {
       marker.setImage(hoverImage);
-
+      if (selectedMarker === marker) {
+        marker.setImage(markerImage);
+        currentOverlay.setMap(null);
+        currentOverlay = null;
+        selectedMarker = null;
+        return;
+      }
       if (selectedMarker !== marker) {
         // selectedMarker가 null이 아닌 경우
         if (!!selectedMarker) {
@@ -37,9 +44,10 @@ const Reservation = () => {
         const overlay = makeOverlay(marker);
         overlay.setMap(studyRoomMap);
         currentOverlay = overlay;
+        selectedMarker = marker;
+        studyRoomMap.panTo(marker.getPosition());
+        return;
       }
-      selectedMarker = marker;
-      studyRoomMap.panTo(marker.getPosition());
     });
 
     kakao.maps.event.addListener(marker, "mouseout", function() {
