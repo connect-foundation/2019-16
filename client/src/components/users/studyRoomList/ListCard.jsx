277173565be4b624e98e5fa1811790f5acbf91f5
aram <arame@kakao.com>
2019-12-13 21:15:42 +0900

    refactor(client:studyroom): 스터디룸 리스트 카드 컴포넌트 리펙토링
    TextInfo컴포넌트 분리로 인한 코드,css 제거
    CardBackground div를 한번 더 감싸 호버 효과를 줌

--- client/src/components/users/studyRoomList/ListCard.jsx
+++ client/src/components/users/studyRoomList/ListCard.jsx
@@ -1,92 +1,70 @@
-import React from "react";
+import React, { Fragment } from "react";
 import styled from "styled-components";
 import TextInfo from "./TextInfo";
 
+const CardBackground = styled.div`
+  :hover {
+    background-color: #f5f5f5;
+  }
+  padding: 0 20px;
+`;
 const Card = styled.div`
-  width: 373px;
+  
   height: 165px;
-  padding: 18px 20px 20px;
+  padding: 18px 0 20px;
   border-top: 1px solid #dfdfdf;
+  justify-content: space-between;
+  $box-radius:: 0px;
+  
+  .card-head {
+    .item-index {
+      color: #00d1b2;
+      font-weight: bold;
+      display: inline-block;
+      padding: 0 5px 0 0;
+    }
+    .studycafe-title {
+      font-weight: bold;
+    }
+  }
+  .card-body {
+    display: flex;
+    justify-content: space-between;
 
-  .item-index {
-    color: #00d1b2;
-    font-weight: bold;
-    display: inline-block;
-    padding: 0 10px 0 0;
-  }
-  .studycafe-title {
-    font-weight: bold;
-  }
-  .text-info-wrapper {
-    .room-name {
-    }
-    .room-info,
-    .cafe-info {
-      font-size: 12px;
-    }
-  }
-  .bold {
-    font-weight: bold;
-  }
-  .img-wrapper {
-    width: 86px;
-    height: 86px;
-    overflow: hidden;
-    img {
+    .img-wrapper {
+      width: 110px;
+      height: 86px;
+      overflow: hidden;
+      display: flex;
+      .img {
+        max-height: 100%;
+      }
     }
   }
 `;
 
+const connectWithMarker = () => {};
 const ListCard = ({ data, index }) => {
-  const {
-    cafe_name,
-    name,
-    price,
-    min_personnel,
-    max_personnel,
-    description,
-    open_time,
-    close_time,
-    images
-  } = data;
+  const { images, cafe_name } = data;
 
   return (
-    <Card>
-      <div>
-        <span className="item-index ">{String.fromCharCode(index)}</span>
-        <span
-          className="studycafe-title"
-          dangerouslySetInnerHTML={{ __html: cafe_name }}
-        ></span>
-      </div>
-      <div className="text-info-wrapper">
-        <div className="room-name">{name}</div>
-        <div className="room-info">
-          <ul>
-            <li>
-              <span className="bold">{"가격 "}</span>
-              {`${price}/1인`}
-            </li>
-            <li>
-              <span className="bold">{"인원 "}</span>
-              {`${min_personnel} ~ ${max_personnel} 명`}
-            </li>
-          </ul>
-          {/* <span>{`가격 ${price}/1인`}</span>
-        <span>{`인원 ${min_personnel} ~ ${max_personnel} 명`}</span> */}
+    <CardBackground className="card-background" onMouseOver={connectWithMarker}>
+      <Card className="card-item">
+        <div className="card-head">
+          <span className="item-index ">{String.fromCharCode(index)}</span>
+          <span
+            className="studycafe-title"
+            dangerouslySetInnerHTML={{ __html: cafe_name }}
+          ></span>
         </div>
-        <div className="cafe-info">
-          <ul>
-            <li>{`매일 ${open_time}:00 ~ ${close_time}:00`}</li>
-            <li></li>
-          </ul>
+        <div className="card-body">
+          <TextInfo data={data} index={index}></TextInfo>
+          <div className="img-wrapper">
+            <img src={images.length > 0 ? images[0] : ""}></img>
+          </div>
         </div>
-      </div>
-
-      <div className="img-wrapper">
-        <img src={images.length > 0 ? images[0] : ""}></img>
-      </div>
-    </Card>
+      </Card>
+    </CardBackground>
   );
 };
 export default ListCard;
