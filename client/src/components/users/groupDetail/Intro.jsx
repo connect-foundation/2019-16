56a8932ee379de8caa7d178aedee8678e7610249
Taehyun Lim <dlatns0201@naver.com>
2019-12-13 19:11:40 +0900

    feat(group_detail): 헤더와 하단의 그룹 소개 컴포넌트
    헤더는 제목과 해당하는 카테고리 버튼이 존재
    그룹 소개는 pre태그를 사용해 newline 적용

---
+++ client/src/components/users/groupDetail/Intro.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledIntro = styled.div`
+  margin: 0.3rem 0;
+  h3 {
+    margin: 2rem 0;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+  .description {
+    font-family: "Nanum Gothic", sans-serif;
+    font-size: 1.5rem;
+    line-height: 2.7rem;
+    padding: 3rem;
+    width: 100%;
+    background-color: whitesmoke;
+    white-space: pre-wrap;
+  }
+`;
+
+const Intro = ({ intro }) => {
+  return (
+    <StyledIntro>
+      <h3> 스터디 소개 </h3>
+      <pre className="description">{intro}</pre>
+    </StyledIntro>
+  );
+};
+
+export default Intro;
