55a1ee6c1e589177ea2933d6cb18b30d7d69b959
Taehyun Lim <dlatns0201@naver.com>
2019-12-13 19:12:39 +0900

    feat(group_detail): 메인 컴포넌트, 그룹 상태 변경 컴포넌트 리뷰 신청
    그룹 상태 변경 컴포넌트의 로직 미완성

---
+++ client/src/components/users/groupDetail/Main.jsx
@@ -0,0 +1,95 @@
+import React, { useCallback } from "react";
+import styled from "styled-components";
+import classnames from "classnames";
+import Subtitle from "../groupCard/Subtitle";
+import Location from "../common/Location";
+import Time from "../groupCard/Time";
+import TagButtons from "../common/TagButtons";
+import ApplyButtons from "../groupDetail/ApplyButtons";
+import { toggle_recruit } from "../../../reducer/users/groupDetail";
+
+const StyledMain = styled.div`
+  width: 100%;
+  height: 23rem;
+  background-color: whitesmoke;
+  * {
+    font-family: "Nanum Gothic", sans-serif;
+    font-size: 1rem;
+  }
+  .imageDiv {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    img {
+      width: 19rem;
+      height: 14rem;
+    }
+  }
+  .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    font-weight: bold;
+    button {
+      font-weight: bold;
+      &.disable {
+        cursor: default;
+        opacity: 0.3;
+      }
+    }
+    p {
+      margin: 0;
+    }
+  }
+  margin: 0;
+  padding: 1.2rem;
+`;
+
+const Main = ({ groupData, dispatch }) => {
+  const {
+    thumbnail,
+    location,
+    startTime,
+    days,
+    endTime,
+    tags,
+    subtitle,
+    min_personnel,
+    now_personnel,
+    max_personnel
+  } = groupData;
+
+  const onToggleReservation = useCallback(isRecruiting => {
+    dispatch(toggle_recruit(isRecruiting));
+  }, []);
+
+  return (
+    <StyledMain className="columns">
+      <div className="column imageDiv is-half">
+        <img src={thumbnail} alt="img" />
+      </div>
+
+      <div className="column content">
+        <Subtitle subtitle={subtitle} />
+        <Location location={location} />
+
+        <h4>
+          <Time startTime={startTime} endTime={endTime} days={days} />
+        </h4>
+
+        <p> 최소 인원: {min_personnel} </p>
+        <p> 현재 인원: {now_personnel} </p>
+        <p> 최대 인원: {max_personnel} </p>
+
+        <TagButtons tags={tags} />
+        <ApplyButtons
+          groupData={groupData}
+          onToggleReservation={onToggleReservation}
+        />
+      </div>
+    </StyledMain>
+  );
+};
+
+export default Main;
