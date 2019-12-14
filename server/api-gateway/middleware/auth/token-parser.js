00eae12d8c99834f95a79988db151d58050a238d
sooob <34637627+soob-forest@users.noreply.github.com>
2019-12-14 21:15:02 +0900

    Revert "Hotfix -> server"

--- server/api-gateway/middleware/auth/token-parser.js
+++ server/api-gateway/middleware/auth/token-parser.js
@@ -1,13 +1,11 @@
 const getRoleFromToken = require("../../lib/getRoleFromToken");
 
-module.exports = function(req, res, next) {
+module.exports = function(req, next) {
   try {
     const jwt = req.cookies.access_token;
     const role = getRoleFromToken(jwt);
 
     req.role = role;
-  } catch (e) {
-    console.error(e);
   } finally {
     next();
   }
