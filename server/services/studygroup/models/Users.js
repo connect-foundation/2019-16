157b26020d9c4ccc4d47ac1c8b846e78591cce24
Sejin Kim <sejjj120@gmail.com>
2019-12-15 20:35:06 +0900

    chore(partnersrv): 해당 브랜치에 불필요한 파일 삭제

--- server/services/studygroup/models/Users.js
+++
@@ -1,9 +0,0 @@
-const { Types, Schema, model } = require("mongoose");
-
-const UserSchema = new Schema({
-  email: { type: String, required: true },
-  name: { type: String, required: true }
-});
-
-exports.schema = UserSchema;
-exports.model = model("User", UserSchema);
