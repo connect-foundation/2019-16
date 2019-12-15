1d79cbc9cfe4e255830e5bea33fe36f282fdd088
Sejin Kim <sejjj120@gmail.com>
2019-12-15 20:23:45 +0900

    chore(gateway): 해당 브랜치에 불필요한 파일 삭제

--- server/services/search/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const { SEARCH_NAME, SEARCH_HOST, SEARCH_PORT } = process.env;
-
-const Search = require("./Search");
-
-const search = new Search(SEARCH_NAME, SEARCH_HOST, SEARCH_PORT);
-
-search.connectToAppListManager();
