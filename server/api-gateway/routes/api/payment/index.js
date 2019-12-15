64504b1d166942383d614b528c817085ebf13d4e
aram <arame@kakao.com>
2019-12-15 20:18:04 +0900

    chore: 로그 서비스 이외의 폴더 삭제

--- server/api-gateway/routes/api/payment/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const inspectPayQueue = require("../../../middleware/payment/inspectPayQueue");
-const readyToPay = require("../../../middleware/payment/readyToPay");
-
-router.post("/ready", inspectPayQueue, readyToPay);
-
-module.exports = router;
