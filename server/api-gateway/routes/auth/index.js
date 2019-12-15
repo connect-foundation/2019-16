c9dd1893c61352b7276bf14dfd0c4a2b9c132a3e
Sejin Kim <sejjj120@gmail.com>
2019-12-15 20:15:53 +0900

    chore(payment): 해당 브랜치에 불필요한 파일 삭제

--- server/api-gateway/routes/auth/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const partnersRouter = require("./partners");
-const usersRouter = require("./users");
-
-router.use("/partners", partnersRouter);
-router.use("/users", usersRouter);
-
-module.exports = router;
--- server/services/applistmanager/index.js
+++
@@ -1,4 +0,0 @@
-require("dotenv").config({ path: ".env" });
-const AppListManager = require("./AppListManager");
-const { ALM_PORT, ALM_HOST } = process.env;
-const appListManager = new AppListManager("AppListManager", ALM_HOST, ALM_PORT);
