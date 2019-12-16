d7110e6c197fc0fc120dc08c2d15e2ef5380833c
Sejin Kim <sejjj120@gmail.com>
2019-12-16 22:37:45 +0900

    feat(gateway): kakao access token 업데이트 미들웨어

--- server/api-gateway/routes/auth/users.js
+++ server/api-gateway/routes/auth/users.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const onlyUser = require("../../middleware/auth/onlyUser");
 const getUserById = require("../../middleware/auth/getUserById");
 const registerFirstTimeUser = require("../../middleware/auth/registerFirstTimeUser");
 const updateKakaoAccessToken = require("../../middleware/auth/updateKakaoAccessToken");
 
 router.get("/accounts/:userId", getUserById);
 router.post("/accounts", registerFirstTimeUser);
-router.patch("/accounts/:userId", onlyUser, updateKakaoAccessToken);
+router.patch("/accounts/:userId", updateKakaoAccessToken);
 
 module.exports = router;
