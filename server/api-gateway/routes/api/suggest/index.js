6a205f7d275a0c7b4bac6ab9632d985aa100cbd3
soob-forest <dltntnqo@naver.com>
2019-12-17 14:18:37 +0900

    feat: 검색어 추천 라우터 추가

---
+++ server/api-gateway/routes/api/suggest/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const { makePacket } = require("../../../../lib/tcp/util");
+
+module.exports = function(apiGateway) {
+  router.post("/availableRooms", (req, res, next) => {
+    const { searchWord } = req.params;
+
+    req.packet = makePacket(
+      "GET",
+      "apigateway",
+      "suggestQueries",
+      "suggestQueries",
+      { searchWord },
+      {},
+      req.resKey,
+      apiGateway.context
+    );
+
+    next();
+  });
+  return router;
+};
