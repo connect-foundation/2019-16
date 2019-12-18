173c7fbbb9cbd1ac7b410fee4d8455b6bb867731
soob-forest <dltntnqo@naver.com>
2019-12-18 21:20:34 +0900

    feat: user 서버 클래스

---
+++ server/services/user/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const App = require("../../lib/tcp/App");
+const { updateJoiningGroups, updateOwnGroups } = require("./query/queries");
+
+const { ACCOUNTS_MONGO_URL } = process.env;
+
+mongoose
+  .connect(ACCOUNTS_MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("User mongoDB is connected");
+  })
+  .catch(err => {
+    console.log("User mongoDB connection fail", err);
+  });
+
+const queryMap = { updateJoiningGroups, updateOwnGroups };
+
+async function doJob(socket, data) {
+  const { params, nextQuery } = data;
+
+  let replyData;
+  let method = "REPLY";
+  let params_ = {};
+  let result;
+
+  try {
+    result = await queryMap[nextQuery](params);
+  } catch (e) {
+    method = "ERROR";
+    result = e;
+  } finally {
+    replyData = {
+      ...data,
+      method,
+      curQuery: nextQuery,
+      params: params_,
+      body: result
+    };
+    const appClient = {};
+
+    this.send(appClient, replyData);
+  }
+}
+
+class User extends App {
+  constructor(name, host, port) {
+    super(name, host, port, doJob);
+  }
+}
+module.exports = User;
