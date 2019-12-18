dbe46b443d11861b0de9ccec78fb31e5db4c82ca
soob-forest <dltntnqo@naver.com>
2019-12-18 19:43:39 +0900

    fix: user스키마 수정
    group을 objectId가 아니라 모델을 가지고 있도록 변경

--- server/api-gateway/models/user.js
+++ server/api-gateway/models/user.js
@@ -1,24 +1,20 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const { StudyGroupSchema } = require("./studygroup");
 
-const UserSchema = new Schema(
-  {
-    userId: String,
-    userEmail: String,
-    userGender: String,
-    userAgeRange: Number,
-    userName: String,
-    kakaoAccessToken: String,
-    profileImage: String,
-    userLocation: { lat: Number, lon: Number },
-    history: [mongoose.Types.ObjectId],
-    ownGroups: [mongoose.Types.ObjectId],
-    partipatedGroups: [mongoose.Types.ObjectId]
-  },
-  {
-    collection: "user"
-  }
-);
+const UserSchema = new Schema({
+  userId: String,
+  userEmail: String,
+  userGender: String,
+  userAgeRange: Number,
+  userName: String,
+  kakaoAccessToken: String,
+  profileImage: String,
+  userLocation: { lat: Number, lon: Number },
+  history: [StudyGroupSchema],
+  ownGroups: [StudyGroupSchema],
+  partipatedGroups: [StudyGroupSchema]
+});
 
 const User = mongoose.model("User", UserSchema);
 
