7aa5972902a38daa24c90646bd714abc67201cbb
soob-forest <dltntnqo@naver.com>
2019-12-18 21:20:01 +0900

    feat: studygroup 스키마 추가

---
+++ server/services/user/model/studygroup.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  email: { type: String, required: true },
+  name: { type: String, required: true }
+});
+
+const GroupSchema = new Schema({
+  title: { type: String },
+  subtitle: { type: String },
+  leader: { type: String },
+  members: [UserSchema],
+  min_personnel: { type: Number },
+  now_personnel: { type: Number },
+  max_personnel: { type: Number },
+  isRecruiting: { type: Boolean },
+  intro: { type: String },
+  thumbnail: { type: String },
+  location: {
+    lat: { type: Number },
+    lon: { type: Number }
+  },
+  category: { type: [String] },
+  tags: { type: [String] },
+  days: { type: [Number] },
+  startTime: { type: Number },
+  endTime: { type: Number }
+});
+
+module.exports = model("StudyGroup", GroupSchema);
