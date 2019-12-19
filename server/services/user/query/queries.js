d3ec2baa9707b2e05f4b828fd8631942ae1f9578
soob-forest <dltntnqo@naver.com>
2019-12-19 18:18:52 +0900

    fix: 그룹 추가인지 삭제인지 구별하기 위한 파라미터 추가

--- server/services/user/query/queries.js
+++ server/services/user/query/queries.js
@@ -1,13 +1,23 @@
 const Users = require("../model/user");
 
-exports.updateJoiningGroups = async ({ userId, joiningGroup }) => {
-  Users.updateOne(
-    { userId: userId },
-    { $push: { joiningGroups: joiningGroup } },
-    err => {
-      if (err) throw new Error(err);
-    }
-  );
+exports.updateJoiningGroups = async ({ userId, joiningGroup, addmode }) => {
+  if (addmode) {
+    Users.updateOne(
+      { userId: userId },
+      { $push: { joiningGroups: joiningGroup } },
+      err => {
+        if (err) throw new Error(err);
+      }
+    );
+  } else {
+    Users.updateOne(
+      { userId: userId },
+      { $pull: { joiningGroups: joiningGroup } },
+      err => {
+        if (err) throw new Error(err);
+      }
+    );
+  }
 };
 
 exports.updateOwnGroups = async ({ userId, ownGroup }) => {
