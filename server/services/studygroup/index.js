ac7e7ea9d5d38b52fe501c146b2e3a27abcdea1a
soob-forest <dltntnqo@naver.com>
2019-12-19 15:07:22 +0900

    feat: usert서비스와 연결 추가

--- server/services/studygroup/index.js
+++ server/services/studygroup/index.js
@@ -22,4 +22,40 @@ const studyGroup = new StudyGroup(
   STUDYGROUP_PORT
 );
 
+async function connectToService(name) {
+  try {
+    let isServiceConnected = false;
+    const service = await this.connectToApp(
+      name,
+      () => {
+        isServiceConnected = true;
+        console.log(`${name} service connect`);
+      },
+      () => {},
+      () => {
+        isServiceConnected = false;
+        console.log(`${name} service end`);
+      },
+      () => {
+        isServiceConnected = false;
+        console.log(`${name} service error`);
+      }
+    );
+
+    setInterval(() => {
+      if (!isServiceConnected) {
+        console.log(`try connect to ${name}`);
+        service.connect();
+      }
+    }, 2000);
+    clearInterval(tryConnect);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 studyGroup.connectToAppListManager();
+
+let tryConnect = setInterval(() => {
+  connectToService.bind(studyGroup)("user");
+}, 1000);
