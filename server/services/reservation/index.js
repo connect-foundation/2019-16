212825bfdb5034cc4ee294c3a7fbaf405c8fe4c2
Sejin Kim <sejjj120@gmail.com>
2019-12-20 03:21:16 +0900

    feat: 결제 서비스의 소켓 클라이언트를 얻기 위한 함수 구현

--- server/services/reservation/index.js
+++ server/services/reservation/index.js
@@ -10,4 +10,41 @@ const reservation = new Reservation(
   RESERVATION_PORT
 );
 
+let tryConnect = setInterval(() => {
+  connectToService.bind(reservation)("payment");
+}, 1000);
+
+async function connectToService(name) {
+  try {
+    let isServiceConnected = false;
+    const service = await this.connectToApp(
+      name,
+      () => {
+        isServiceConnected = true;
+        console.log(`${name} service connect`);
+      },
+      () => {},
+      () => {
+        isServiceConnected = false;
+        console.log(`${name} service end`);
+      },
+      () => {
+        isServiceConnected = false;
+        console.log(`${name} service error`);
+      }
+    );
+
+    setInterval(() => {
+      if (!isServiceConnected) {
+        console.log(`try connect to ${name}`);
+        service.connect();
+      }
+    }, 2000);
+
+    clearInterval(tryConnect);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 reservation.connectToAppListManager();
