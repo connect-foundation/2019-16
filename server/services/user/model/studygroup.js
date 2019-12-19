68a6b0345deddfc5afe3cabdeb20017ee2025941
soob-forest <dltntnqo@naver.com>
2019-12-19 15:13:25 +0900

    fix: 유저 스키마 변경
    _id 옵션 false

--- server/services/user/model/studygroup.js
+++ server/services/user/model/studygroup.js
@@ -5,26 +5,29 @@ const UserSchema = new Schema({
   name: { type: String, required: true }
 });
 
-const GroupSchema = new Schema({
-  title: { type: String },
-  subtitle: { type: String },
-  leader: { type: String },
-  members: [UserSchema],
-  min_personnel: { type: Number },
-  now_personnel: { type: Number },
-  max_personnel: { type: Number },
-  isRecruiting: { type: Boolean },
-  intro: { type: String },
-  thumbnail: { type: String },
-  location: {
-    lat: { type: Number },
-    lon: { type: Number }
+const GroupSchema = new Schema(
+  {
+    title: { type: String },
+    subtitle: { type: String },
+    leader: { type: String },
+    members: [UserSchema],
+    min_personnel: { type: Number },
+    now_personnel: { type: Number },
+    max_personnel: { type: Number },
+    isRecruiting: { type: Boolean },
+    intro: { type: String },
+    thumbnail: { type: String },
+    location: {
+      lat: { type: Number },
+      lon: { type: Number }
+    },
+    category: { type: [String] },
+    tags: { type: [String] },
+    days: { type: [Number] },
+    startTime: { type: Number },
+    endTime: { type: Number }
   },
-  category: { type: [String] },
-  tags: { type: [String] },
-  days: { type: [Number] },
-  startTime: { type: Number },
-  endTime: { type: Number }
-});
+  { _id: false }
+);
 
 module.exports = model("StudyGroup", GroupSchema);
