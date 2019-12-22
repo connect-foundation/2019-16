73b4f38d6fee469a2addff54180a5343cc6a02a2
plm9606 <plm9606@naver.com>
2019-12-22 20:08:34 +0900

    feat(client/reservation): 맵 사이드바 클릭이벤트 생성
    클릭하면 해당 좌표로 지도가 포커스된다.

--- client/src/components/users/studyRoomList/ListCard.jsx
+++ client/src/components/users/studyRoomList/ListCard.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import TextInfo from "./TextInfo";
 import { UserContext } from "../../../pages/users/index";
 import { useContext } from "react";
+import { hoverImage } from "../../../lib/kakaoMapUtils";
+const { kakao } = window;
 
 const CardBackground = styled.div`
   :hover {
@@ -46,14 +48,19 @@ const Card = styled.div`
 `;
 
 const ListCard = ({ data, index, error }) => {
-  const { images, cafe_name } = data;
   const { map } = useContext(UserContext);
+  const { images, cafe_name, marker } = data;
   const openInfowindow = useCallback(() => {
     data.marker.infowindow_over.call();
-  }, []);
+  }, [marker, data]);
   const closeInfowindow = useCallback(() => {
     data.marker.infowindow_out.call();
-  }, []);
+  }, [marker, data]);
+
+  const clickMarker = useCallback(() => {
+    map.current.setLevel(3);
+    map.current.panTo(data.marker.getPosition());
+  }, [map]);
 
   return (
     <CardBackground className="card-background">
@@ -61,6 +68,7 @@ const ListCard = ({ data, index, error }) => {
         className="card-item"
         onMouseOver={openInfowindow}
         onMouseOut={closeInfowindow}
+        onClick={clickMarker}
       >
         <div className="card-head">
           <span className="item-index ">{String.fromCharCode(index)}</span>
