cd6c02f4d4cd33dd0453f83594e960ed980ca4a4
Sejin Kim <sejjj120@gmail.com>
2019-12-23 01:24:52 +0900

    feat: 불필요한 라우터 삭제
    kakao access token을 갈아 끼우는 기능은 현재로서는 불필요하기 때문에 삭제.

--- server/api-gateway/routes/auth/users.js
+++ server/api-gateway/routes/auth/users.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const removeCookie = require("../../middleware/auth/removeCookie");
 const getUserById = require("../../middleware/auth/getUserById");
 const registerFirstTimeUser = require("../../middleware/auth/registerFirstTimeUser");
-const updateKakaoAccessToken = require("../../middleware/auth/updateKakaoAccessToken");
 
 router.get("/logout", removeCookie);
 router.get("/accounts/:userId", getUserById);
 router.post("/accounts", registerFirstTimeUser);
-router.patch("/accounts/:userId", updateKakaoAccessToken);
 
 module.exports = router;
