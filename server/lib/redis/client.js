04531522b03e6e18b25f40a5ea5a4242661596a6
sooob <dltntnqo@naver.com>
2019-12-28 15:34:14 +0900

    fix: 캐시서버 호스트 하드코딩 수정
    하드코딩 되있었던 host CACHE_HOST로 변경

--- server/lib/redis/client.js
+++ server/lib/redis/client.js
@@ -1,7 +1,6 @@
 const redis = require("redis");
-// const client = redis.createClient(6379, "106.10.57.60");
 const client = redis.createClient();
 const multi = client.multi();
-const cacheClient = redis.createClient(6379, "106.10.57.60");
+const cacheClient = redis.createClient(6379, CACHE_HOST);
 
 module.exports = { client, multi, cacheClient };
