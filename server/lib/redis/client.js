a775505d8483eb3703c877a6f57c415291e61152
plm9606 <plm9606@naver.com>
2020-05-31 23:30:53 +0900

    refactor: redis url 환경변수 주입
    (cherry picked from commit 494bed273b70d590c6d549a9cfcfd9223f3b00b0)

--- server/lib/redis/client.js
+++ server/lib/redis/client.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+const { REDIS_PORT, REDIS_URL } = process.env;
 const redis = require("redis");
-//const client = redis.createClient(6379, "106.10.57.60");
-const client = redis.createClient();
+const client = redis.createClient(REDIS_PORT, REDIS_URL);
+// const client = redis.createClient();
 const multi = client.multi();
 
 module.exports = { client, multi };
